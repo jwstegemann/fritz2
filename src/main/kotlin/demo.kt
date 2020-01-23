257cb7f7f931fdb90b712eac82064d96380d9b58
Jan Weidenhaupt <jan.weidenhaupt@oeffentliche.de>
2020-01-23 22:37:43 +0100

    extend demo app

--- src/main/kotlin/demo.kt
+++ src/main/kotlin/demo.kt
@@ -1,9 +1,13 @@
 import io.fritz2.binding.Store
+import io.fritz2.binding.not
 import io.fritz2.dom.html.html
 import io.fritz2.dom.mount
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.map
+import org.w3c.dom.events.Event
+import org.w3c.dom.events.FocusEvent
+import org.w3c.dom.events.MouseEvent
 
 
 data class ActionData(val x: Int, val y: Int)
@@ -16,6 +20,14 @@ fun main() {
         val addADot = Handler<ActionData> { model, _ ->
             "$model."
         }
+        val inputHasFocus = Handler<FocusEvent> { model, _ ->
+            console.log("input has now focus")
+            model
+        }
+        val divClicked = Handler<MouseEvent> { model, _ ->
+            console.log("div clicked")
+            model
+        }
     }
 
     val myComponent = html {
@@ -23,10 +35,14 @@ fun main() {
             input() {
                 value = store.data
                 store.update <= changes
+                type = !"text"
+                maxLength = !10
+                store.inputHasFocus <= focuss
             }
             div {
                 +"value: "
                 store.data.bind()
+                store.divClicked <= clicks
             }
             button {
                 +"add one more little dot"
