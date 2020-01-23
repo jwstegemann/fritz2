e1f014c0a9f03b55a7b9bdfeaa8d77fd4725b166
Jan Weidenhaupt <jan.weidenhaupt@oeffentliche.de>
2020-01-23 22:34:54 +0100

    added all attributes for div, input and button

--- src/main/kotlin/io/fritz2/dom/html/elements.kt
+++ src/main/kotlin/io/fritz2/dom/html/elements.kt
@@ -6,30 +6,71 @@ import io.fritz2.dom.WithText
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
-import org.w3c.dom.Element
-import org.w3c.dom.HTMLButtonElement
-import org.w3c.dom.HTMLDivElement
-import org.w3c.dom.HTMLInputElement
-import org.w3c.dom.events.MouseEvent
+import org.w3c.dom.*
+import kotlin.js.Date
 
 @ExperimentalCoroutinesApi
 @FlowPreview
-class Div(): Tag<HTMLDivElement>("div"), WithText<HTMLDivElement>
+class Div(): Tag<HTMLDivElement>("div"), WithText<HTMLDivElement> {
+    var align: Flow<String> by AttributeDelegate
+}
 
 //FIXME: use correct type for domNode - HtmlButtonElement here
 @ExperimentalCoroutinesApi
 @FlowPreview
 class Button(): Tag<HTMLButtonElement>("button"), WithText<HTMLButtonElement> {
-    //TODO: structure attributes and events in interfaces
-    val clicks by lazy { subscribe(Click) }
+    var autofocusFlow: Flow<Boolean> by AttributeDelegate
+    var disabledFlow: Flow<Boolean> by AttributeDelegate
+    var formAction: Flow<String> by AttributeDelegate
+    var formEnctype: Flow<String> by AttributeDelegate
+    var formMethod: Flow<String> by AttributeDelegate
+    var formNoValidate: Flow<Boolean> by AttributeDelegate
+    var formTarget: Flow<String> by AttributeDelegate
+    var name: Flow<String> by AttributeDelegate
+    var type: Flow<String> by AttributeDelegate
+    var value: Flow<String> by AttributeDelegate
+    
 }
 
 @ExperimentalCoroutinesApi
 @FlowPreview
 class Input(): Tag<HTMLInputElement>("input") {
+    var accept: Flow<String> by AttributeDelegate
+    var alt: Flow<String> by AttributeDelegate
+    var autocomplete: Flow<String> by AttributeDelegate
+    var autofocus: Flow<Boolean> by AttributeDelegate
+    var defaultChecked: Flow<Boolean> by AttributeDelegate
+    var checked: Flow<Boolean> by AttributeDelegate
+    var dirName: Flow<String> by AttributeDelegate
+    var disabled: Flow<Boolean> by AttributeDelegate
+    var formAction: Flow<String> by AttributeDelegate
+    var formEnctype: Flow<String> by AttributeDelegate
+    var formMethod: Flow<String> by AttributeDelegate
+    var formNoValidate: Flow<Boolean> by AttributeDelegate
+    var formTarget: Flow<String> by AttributeDelegate
+    var height: Flow<Int> by AttributeDelegate
+    var indeterminate: Flow<Boolean> by AttributeDelegate
+    var inputMode: Flow<String> by AttributeDelegate
+    var max: Flow<String> by AttributeDelegate
+    var maxLength: Flow<Int> by AttributeDelegate
+    var min: Flow<String> by AttributeDelegate
+    var minLength: Flow<Int> by AttributeDelegate
+    var multiple: Flow<Boolean> by AttributeDelegate
+    var name: Flow<String> by AttributeDelegate
+    var pattern: Flow<String> by AttributeDelegate
+    var placeholder: Flow<String> by AttributeDelegate
+    var readOnly: Flow<Boolean> by AttributeDelegate
+    var required: Flow<Boolean> by AttributeDelegate
+    var size: Flow<Int> by AttributeDelegate
+    var src: Flow<String> by AttributeDelegate
+    var step: Flow<String> by AttributeDelegate
+    var type: Flow<String> by AttributeDelegate
+    var defaultValue: Flow<String> by AttributeDelegate
     var value: Flow<String> by AttributeDelegate
-
-    val changes by lazy { subscribe(Change) }
+    var valueAsDate: Flow<Date> by AttributeDelegate
+    var valueAsNumber: Flow<Double> by AttributeDelegate
+    var width: Flow<Int> by AttributeDelegate
+    var align: Flow<String> by AttributeDelegate
 }
 
 @ExperimentalCoroutinesApi
