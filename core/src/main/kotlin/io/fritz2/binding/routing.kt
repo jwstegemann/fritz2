410d54af8b1c17cb01d799bae54cf2f0bd3813f9
Jan Weidenhaupt <jan.weidenhaupt@oeffentliche.de>
2020-02-12 22:15:02 +0100

    new version of routing

--- core/src/main/kotlin/io/fritz2/binding/routing.kt
+++ core/src/main/kotlin/io/fritz2/binding/routing.kt
@@ -3,8 +3,10 @@ package io.fritz2.binding
 import io.fritz2.dom.html.Events
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import org.w3c.dom.events.Event
 import kotlin.browser.window
 
@@ -13,49 +15,85 @@ external fun encodeURIComponent(decodedURI: String): String
 
 @FlowPreview
 @ExperimentalCoroutinesApi
-object Routing {
+fun routing(default: String = ""): Router<String> = object : Router<String>(default) {
+    override fun unmarshal(hash: String): String = hash
+    override fun marshal(route: String): String = route
+}
 
-    private val divider = "&"
-    private val assignment = "="
+@FlowPreview
+@ExperimentalCoroutinesApi
+fun routing(default: Map<String, String> = emptyMap()) = RouterWithMap(default)
 
-    private var current: Map<String, String>
-    init {
-        val hash = window.document.location?.hash
-        current = if(hash != null) unmarshal(hash) else emptyMap()
-    }
+//TODO add router for data classes
 
-    private val flow: Flow<Map<String, String>> = callbackFlow {
+/**
+ * Router register the event-listener for hashchange-event and
+ * handles route-changes. Therefore it uses [marshal] and [unmarshal]
+ *
+ * @param T type to marshal to
+ * @property initialData default value
+ */
+@FlowPreview
+@ExperimentalCoroutinesApi
+abstract class Router<T>(initialData: T) {
+    private val prefix = "#"
+
+    private val updates: Flow<Update<T>> = callbackFlow {
         val listener: (Event) -> Unit = {
-            val hash = window.document.location?.hash
-            if(hash != null && hash.isNotBlank()) {
-                current = unmarshal(hash)
-                channel.offer(current)
+            it.preventDefault()
+            val hash = window.document.location?.hash?.removePrefix(prefix)
+            if (hash != null && hash.isNotBlank()) {
+                channel.offer { unmarshal(decodeURIComponent(hash)) }
             }
         }
+        window.addEventListener(Events.load.name, listener)
         window.addEventListener(Events.hashchange.name, listener)
 
         awaitClose { window.removeEventListener(Events.hashchange.name, listener) }
     }
+    private val applyUpdate: suspend (T, Update<T>) -> T = { lastValue, update -> update(lastValue) }
 
-    val params = flow.distinctUntilChanged()
+    val data: Flow<T> = updates.scan(initialData, applyUpdate).distinctUntilChanged()
 
-    fun set(key: String, value: String) {
-        current += key to value
-        window.location.hash = marshal(current)
+    abstract fun unmarshal(hash: String): T
+    abstract fun marshal(route: T): String
+
+    //FIXME not working yet... always getting a full page reload!?
+    val to: Handler<T> = Handler { route ->
+        val newRoute = prefix + encodeURIComponent(marshal(route))
+        console.log("change hash to: $newRoute")
+        window.location.hash = newRoute
+        route
     }
 
-    fun setAll(all: Map<String, String>) {
-        current = all
-        window.location.hash = marshal(current)
+    inner class Handler<T>(inline val handler: (T) -> T) {
+        private fun handle(action: Flow<T>) {
+            GlobalScope.launch {
+                action.collect {
+                    handler(it)
+                }
+            }
+        }
+
+        // syntactical sugar to write handler <= event-stream
+        operator fun compareTo(action: Flow<T>): Int {
+            handle(action)
+            return 0
+        }
     }
+}
 
-    fun select(key: String): Flow<Pair<String, Map<String, String>>> = params.map{m -> (m[key] ?: "") to m}
+@FlowPreview
+@ExperimentalCoroutinesApi
+class RouterWithMap(default: Map<String, String>) : Router<Map<String, String>>(default) {
+    private val assignment = "="
+    private val divider = "&"
 
-    private fun marshal(params: Map<String, String>): String =
-        params.map { (key, value) -> "$key$assignment${encodeURIComponent(value)}" }.joinToString(separator = divider)
+    override fun unmarshal(hash: String): Map<String, String> =
+        hash.split(divider).filter { s -> s.isNotBlank() }.asReversed().map(::extractPair).toMap()
 
-    private fun unmarshal(params: String): Map<String, String> =
-        params.drop(1).split(divider).filter{s -> s.isNotBlank()}.asReversed().map(::extractPair).toMap()
+    override fun marshal(route: Map<String, String>): String =
+        route.map { (key, value) -> "$key$assignment${encodeURIComponent(value)}" }.joinToString(separator = divider)
 
     private fun extractPair(param: String): Pair<String, String> {
         val equalsPos = param.indexOf(assignment)
@@ -63,7 +101,9 @@ object Routing {
             val key = param.substring(0, equalsPos)
             val value = decodeURIComponent(param.substring(equalsPos + 1))
             key to value
-        }
-        else param to "true"
+        } else param to "true"
     }
+
+    fun <X> select(key: String, mapper: (Pair<String, Map<String, String>>) -> X): Flow<X> =
+        data.map { m -> mapper((m[key] ?: "") to m) }
 }
--- examples/routing/src/main/kotlin/io/fritz2/examples/routing/routing.kt
+++ examples/routing/src/main/kotlin/io/fritz2/examples/routing/routing.kt
@@ -5,35 +5,72 @@ import io.fritz2.dom.html.html
 import io.fritz2.dom.mount
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.map
 
+object Pages {
+    const val a = "pageA"
+    const val b = "pageB"
+    const val c = "pageC"
+}
 
 @ExperimentalCoroutinesApi
 @FlowPreview
 fun main() {
 
-    val store = object : RootStore<String>("") {
-
-        val changePage = Handler<String> { model, newPage ->
-            Routing.set("page", newPage)
-            model
-        }
-
-    }
+    val router = routing(mapOf("page" to Pages.b, "test" to "=&%#+!§/%\\\$&()"))
 
     val myComponent = html {
         section {
-            h1 {
-                +"Page: "
-                +Routing.listenFor("page")
-            }
-            label {
-                +"New Page"
-                input {
-                    store.changePage <= changes
+            ul {
+                li {
+                    a {
+                        +"Show ${Pages.a}"
+                        href = !""
+                        router.to <= clicks.map { mapOf("page" to Pages.a) } //FIXME not working!?
+                    }
                 }
+                li {
+                    a {
+                        +"Show ${Pages.b}"
+                        href = !""
+                        router.to <= clicks.map { mapOf("page" to Pages.b) } //FIXME not working!?
+                    }
+                }
+                li {
+                    a {
+                        +"Show ${Pages.c}"
+                        href = !"#page=${Pages.c}" //works
+                    }
+                }
+            }
+            div {
+                router.select("page") {
+                    val (page, params) = it
+                    when (page) {
+                        Pages.a -> html {
+                            h1 {
+                                +"Showing page A"
+                            }
+                        }
+                        Pages.b -> html {
+                            h1 {
+                                +"Showing page B"
+                            }
+                        }
+                        Pages.c -> html {
+                            h1 {
+                                +"Showing page C"
+                            }
+                        }
+                        else -> html {
+                            h1 {
+                                +"Page $page not found!"
+                            }
+                        }
+                    }
+                }.bind()
             }
         }
     }
-
     myComponent.mount("target")
 }
