9b245408f15b9f3e6088a8840fe48dc7a3d0f8ff
Jens Stegemann <jens.stegemann@oeffentliche.de>
2020-02-13 21:23:31 +0100

    post, error-handling

--- core/src/main/kotlin/io/fritz2/remote/remote.kt
+++ core/src/main/kotlin/io/fritz2/remote/remote.kt
@@ -2,21 +2,26 @@ package io.fritz2.remote
 
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import org.w3c.dom.url.URL
+import org.w3c.fetch.Headers
 import org.w3c.fetch.RequestInit
 import org.w3c.fetch.Response
 import kotlin.browser.window
 
 
+
 @ExperimentalCoroutinesApi
 fun URL.get() = flow {
-    emit(window.fetch(this@get, RequestInit(
+    val response = window.fetch(this@get, RequestInit(
         method = "GET"
-    )).await())
-}
+    )).await()
 
+    if (response.ok) emit(response)
+    else throw FetchException(response.status, response.text().await())
+}
 
 fun URL.post(body: String)= flow {
     emit(window.fetch(this@post, RequestInit(
@@ -25,9 +30,13 @@ fun URL.post(body: String)= flow {
     )).await())
 }
 
-
-//TODO: error-handling
-
 fun Flow<Response>.body() = this.map {
     it.text().await()
+}
+
+class FetchException(val statusCode: Short, val body: String) : Exception()
+
+fun Flow<Response>.onError(handler: (FetchException) -> Unit) = this.catch { e ->
+    if (e is FetchException) handler(e)
+    else throw e
 }
--- examples/remote/src/main/kotlin/io/fritz2/examples/remote/remote.kt
+++ examples/remote/src/main/kotlin/io/fritz2/examples/remote/remote.kt
@@ -5,6 +5,8 @@ import io.fritz2.dom.html.html
 import io.fritz2.dom.mount
 import io.fritz2.remote.body
 import io.fritz2.remote.get
+import io.fritz2.remote.onError
+import io.fritz2.remote.post
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import org.w3c.dom.url.URL
@@ -21,7 +23,17 @@ fun main() {
     val store = object : RootStore<String>("start") {
 
         val callApi = apply { s : String ->
-            URL("http://localhost:9000/get?foo=$s").get().body()
+            URL("https://reqres.in/api/users/$s").get().onError { window.alert("error fetching data: ${it.statusCode}, ${it.body}") }
+                .body()
+        } andThen update
+
+        val postUpdate = apply {s : String ->
+            URL("https://reqres.in/api/users").post("""
+                {
+                    "name": "$s",
+                    "job": "leader"
+                }
+            """.trimIndent()).body()
         } andThen update
 
     }
@@ -30,7 +42,7 @@ fun main() {
         div {
             input {
                 value = !"hgfhgfhgf"
-                store.callApi <= changes
+                store.postUpdate <= changes
             }
             div {
                 +"value: "
