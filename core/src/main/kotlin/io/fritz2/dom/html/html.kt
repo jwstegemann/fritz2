d3b34749988b8832477df46700926171c4b616db
Jan Weidenhaupt <jan.weidenhaupt@oeffentliche.de>
2020-03-03 19:07:13 +0100

    throw exception when tried to render multiple root tags

--- core/src/main/kotlin/io/fritz2/dom/html/html.kt
+++ core/src/main/kotlin/io/fritz2/dom/html/html.kt
@@ -9,8 +9,20 @@ import org.w3c.dom.Element
 @FlowPreview
 fun <E : Element> html(content: HtmlElements.() -> Tag<E>) =
     content(object : HtmlElements {
+
+        var alreadyRegistered: Boolean = false
+
         override fun <X : Element, T : Tag<X>> register(element: T, content: (T) -> Unit): T {
-            content(element)
-            return element
+            if (!alreadyRegistered) {
+                content(element)
+                alreadyRegistered = true
+                return element
+            } else {
+                throw MultipleRootElementsException(
+                    "Don't use multiple root elements/tags in the html{...} context of your component! That's why rendering is not working."
+                )
+            }
         }
     })
+
+internal class MultipleRootElementsException(message: String) : RuntimeException(message)
