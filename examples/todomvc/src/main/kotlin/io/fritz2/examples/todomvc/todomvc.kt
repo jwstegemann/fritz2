279b4ceb808d2a23e9a25be4be20b372d1339733
jwstegemann <jwstegemann@gmail.com>
2020-03-22 15:48:47 +0100

    continued todomvc

--- examples/todomvc/src/main/kotlin/io/fritz2/examples/todomvc/todomvc.kt
+++ examples/todomvc/src/main/kotlin/io/fritz2/examples/todomvc/todomvc.kt
@@ -1,20 +1,135 @@
 package io.fritz2.examples.todomvc
 
+import io.fritz2.binding.Const
+import io.fritz2.binding.RootStore
+import io.fritz2.binding.each
+import io.fritz2.binding.eachStore
+import io.fritz2.dom.html.HtmlElements
 import io.fritz2.dom.html.html
 import io.fritz2.dom.mount
+import io.fritz2.dom.states
+import io.fritz2.dom.values
+import io.fritz2.optics.WithId
+import io.fritz2.optics.buildLens
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+
+data class ToDo(
+    val text: String,
+    val completed: Boolean,
+    override val id: String = text.hashCode().toString()
+) : WithId
+
+val textLens = buildLens<ToDo, String>("text", {it.text}, {p,v -> p.copy(text = v)})
+val completedLens = buildLens<ToDo, Boolean>("completed", {it.completed}, {p,v -> p.copy(completed = v)})
 
 
 @ExperimentalCoroutinesApi
 @FlowPreview
 fun main() {
 
-    val myComponent = html {
-        div {
-            +"Here we go again!"
+    val toDos = object : RootStore<List<ToDo>>(emptyList()) {
+        val add = handle<String> { toDos, text ->
+            toDos + ToDo(text, false)
+        }
+
+        val count = data.map { it.size }
+    }
+
+    fun HtmlElements.inputHeader() {
+        header {
+            h1 { +"todos" }
+            input {
+                className = !"new-todo"
+                placeholder = !"What needs to be done?"
+                autofocus = Const(true)
+
+                toDos.add <= changes.values().onEach {
+                    console.log("adding $it")
+                    domNode.value = ""
+                }
+            }
         }
     }
 
-    myComponent.mount("target")
+    fun HtmlElements.mainSection() {
+        section {
+            className = !"main"
+            //        <input id="toggle-all" class="toggle-all" type="checkbox">
+            //        <label for="toggle-all">Mark all as complete</label>
+            ul {
+                className = !"todo-list"
+                toDos.eachStore().map { toDoStore ->
+                    val textStore = toDoStore.sub(textLens)
+                    val completedStore = toDoStore.sub(completedLens)
+                    html {
+                        li {
+                            className = completedStore.data.map { if(it) "completed" else "" }
+                            div {
+                                className = !"view"
+                                input {
+                                    className = !"toggle"
+                                    type = !"checkbox"
+                                    defaultChecked = completedStore.data
+                                    completedStore.update <= changes.states()
+                                }
+                                label { textStore.data.bind() }
+                                button {
+                                    className = !"destroy"
+                                }
+                            }
+                            // <input class="edit" value="Create a TodoMVC template">
+                        }
+                    }
+                }.bind()
+            }
+        }
+    }
+
+    fun HtmlElements.appFooter() {
+        footer {
+            className = !"footer"
+            span {
+                className = !"todo-count"
+                strong {
+                    toDos.count.map {
+                        val plural = if (it != 1) "s" else ""
+                        "$it item$plural left"
+                    }.bind()
+                }
+            }
+        }
+        /*
+        <footer class="footer">
+        <!-- This should be `0 items left` by default -->
+        <span class="todo-count"><strong>0</strong> item left</span>
+        <!-- Remove this if you don't implement routing -->
+        <ul class="filters">
+        <li>
+        <a class="selected" href="#/">All</a>
+        </li>
+        <li>
+        <a href="#/active">Active</a>
+        </li>
+        <li>
+        <a href="#/completed">Completed</a>
+        </li>
+        </ul>
+        <!-- Hidden if no completed items are left ↓ -->
+        <button class="clear-completed">Clear completed</button>
+        </footer>
+         */
+    }
+
+    val app = html {
+        section {
+            inputHeader()
+            mainSection()
+            appFooter()
+        }
+    }
+
+    app.mount("todoapp")
 }
