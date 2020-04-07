8e6fc157ba029e5c85b57c68f3fc1c0bb272ac49
Jan Weidenhaupt <jan.weidenhaupt@oeffentliche.de>
2020-04-07 13:36:55 +0200

    reworked nestedmodel example

--- examples/nestedmodel/model/src/main/kotlin/io/fritz2/examples/nestedmodel/model.kt
+++ examples/nestedmodel/model/src/main/kotlin/io/fritz2/examples/nestedmodel/model.kt
@@ -4,13 +4,32 @@ import io.fritz2.optics.Lenses
 import io.fritz2.optics.WithId
 
 @Lenses
-data class Outer(val inner: Inner, val value: String, val seq: List<Element>) {
-}
+data class Person(
+    override val id: String,
+    val name: String = "",
+    val birthday: String = "",
+    val address: Address = Address(),
+    val activities: List<Activity> = listOf(
+        Activity("ac1", "walking"),
+        Activity("ac2", "running"),
+        Activity("ac3", "meeting friends"),
+        Activity("ac4", "playing computer games"),
+        Activity("ac5", "programming"),
+        Activity("ac6", "to go biking")
+    )
+) : WithId
 
 @Lenses
-data class Inner(val value: String)
+data class Address(
+    val street: String = "",
+    val number: String = "",
+    val postalCode: String = "",
+    val city: String = ""
+)
 
 @Lenses
-data class Element(val value: String, override val id: String) : WithId
-
-val test = ""
+data class Activity(
+    override val id: String,
+    val name: String,
+    val like: Boolean = false
+) : WithId
--- examples/nestedmodel/src/main/kotlin/io/fritz2/examples/nestedmodel/nestedmodel.kt
+++ examples/nestedmodel/src/main/kotlin/io/fritz2/examples/nestedmodel/nestedmodel.kt
@@ -1,67 +1,151 @@
 package io.fritz2.examples.nestedmodel
 
-import io.fritz2.binding.RootStore
-import io.fritz2.binding.eachStore
+import io.fritz2.binding.*
+import io.fritz2.dom.Tag
+import io.fritz2.dom.html.HtmlElements
 import io.fritz2.dom.html.html
 import io.fritz2.dom.mount
+import io.fritz2.dom.states
 import io.fritz2.dom.values
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flatMapMerge
 import kotlinx.coroutines.flow.map
+import org.w3c.dom.HTMLDivElement
 
 @ExperimentalCoroutinesApi
 @FlowPreview
 fun main() {
 
-    val model = RootStore(Outer(Inner("hello"), "world", listOf(
-        Element("one","n1"),
-        Element("two","n2"),
-        Element("three","n3")
-    )))
-    val outerValue = model.sub(Lenses.Outer.value)
-    val innerValue = model.sub(Lenses.Outer.inner).sub(Lenses.Inner.value)
-    val seq = model.sub(Lenses.Outer.seq)
+    val personStore = RootStore(Person(uniqueId()))
 
-    val myComponent = html {
-        div {
-            input {
-                value = outerValue.data
-                outerValue.update <= changes.values()
+    val name = personStore.sub(Lenses.Person.name)
+    val birthday = personStore.sub(Lenses.Person.birthday)
+    val address = personStore.sub(Lenses.Person.address)
+    val street = address.sub(Lenses.Address.street)
+    val number = address.sub(Lenses.Address.number)
+    val postalCode = address.sub(Lenses.Address.postalCode)
+    val city = address.sub(Lenses.Address.city)
+    val activities = personStore.sub(Lenses.Person.activities)
+
+    val listStore = object : RootStore<List<Person>>(emptyList()) {
+        val add: Handler<Person> = handle { list, person ->
+            list + person
+        }
+    }
+
+    // helper method for creating form-groups from SubStores
+    fun <X, Y> HtmlElements.formGroup(label: String,
+                                      subStore: SubStore<X, Y, String>,
+                                      inputType: String = "text",
+                                      extraClass: String = "") {
+        div("form-group $extraClass") {
+            label {
+                text(label)
+                `for` = const(subStore.id)
             }
-            span {
-                text("Outer.value = ")
-                outerValue.data.bind()
-            }
-            input {
-                value = innerValue.data
-                innerValue.update <= changes.values()
-            }
-            span {
-                text("Inner.value = ")
-                innerValue.data.bind()
-            }
-            ul {
-                seq.eachStore().map {
-                    html {
-                        val elementValue = it.sub(Lenses.Element.value)
-                        li {
-                            input(id = it.id) {
-                                value = elementValue.data
-                                elementValue.update <= changes.values()
-                            }
-                        }
-                    }
-                }.bind()
-            }
-            div {
-                text("Model = ")
-                model.data.map {
-                    it.toString()
-                }.bind()
+            input("form-control", id = subStore.id) {
+                placeholder = const(label)
+                value = subStore.data
+                type = const(inputType)
+
+                subStore.update <= changes.values()
             }
         }
     }
 
-    myComponent.mount("target")
+    fun HtmlElements.activityCheckbox(activity: SubStore<Person, List<Activity>, Activity>): Tag<HTMLDivElement> {
+        val name = activity.sub(Lenses.Activity.name)
+        val like = activity.sub(Lenses.Activity.like)
+
+        return html {
+            div("form-check form-check-inline") {
+                input("form-check-input", id = activity.id) {
+                    type = const("checkbox")
+                    checked = like.data
+
+                    like.update <= changes.states()
+                }
+                label("form-check-label") {
+                    `for` = const(activity.id)
+
+                    name.data.bind()
+                }
+            }
+        }
+    }
+
+    html {
+        div {
+            h4 { text("Person") }
+            formGroup("Name", name)
+            formGroup("Birthday", birthday, "date")
+            div("form-row") {
+                formGroup("Street", street, extraClass = "col-md-6")
+                formGroup("House Number", number, extraClass = "col-md-6")
+            }
+            div("form-row") {
+                formGroup("Postal Code", postalCode, extraClass = "col-md-6")
+                formGroup("City", city, extraClass = "col-md-6")
+            }
+            div("form-row") {
+                div("form-group") {
+                    activities.eachStore().map { activity ->
+                        activityCheckbox(activity)
+                    }.bind()
+                }
+            }
+            div("form-group my-4") {
+                button("btn btn-primary") {
+                    text("Add")
+                    listStore.add <= clicks.events.flatMapMerge { personStore.data }
+                }
+
+                button("btn btn-secondary mx-2") {
+                    text("Show data")
+                    attr("data-toggle", "collapse")
+                    attr("data-target", "#showData")
+                }
+                div("collapse", id = "showData") {
+                    div("card card-body") {
+                        pre {
+                            code {
+                                personStore.data.map { JSON.stringify(it, space = 2) }.bind()
+                            }
+                        }
+                    }
+                }
+            }
+
+            hr("my-4") { }
+
+            table("table") {
+                thead("thead-dark") {
+                    th { text("Name") }
+                    th { text("Birthday") }
+                    th { text("Address") }
+                    th { text("Activities") }
+                }
+                tbody {
+                    listStore.data.each().map { person ->
+                        val address = "${person.address.street} ${person.address.number}, " +
+                                "${person.address.postalCode} ${person.address.city}"
+                        val activities = person.activities.filter { it.like }.map { it.name }.joinToString()
+
+                        html {
+                            tr {
+                                td { text(person.name) }
+                                td { text(person.birthday) }
+                                td { text(address) }
+                                td { text(activities) }
+                            }
+                        }
+                    }.bind()
+                }
+            }
+        }
+    }.mount("target")
 
 }
