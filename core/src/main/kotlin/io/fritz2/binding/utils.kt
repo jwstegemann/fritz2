6afdc867724762b3fe2cf7e6d448b2c478cb7aa6
Jens <jwstegemann@gmail.com>
2020-04-08 22:15:51 +0200

    UUID (#54)
    * added extra value attribute MountPoint and Delegate
    
    * styled gettingstarted example with source code integration
    
    * changed label name
    
    * Test for Myer
    
    * random tests working for Myer
    
    * combined Deletes and Moves
    
    * InsertMany
    
    * running tests and examples except todomvc
    
    * console.logs removed
    
    * todomvc working
    
    * tidy up
    
    * added source ref to todomvc-page
    
    * added function to create uuid
    
    * removed old id-function
    
    Co-authored-by: Jan Weidenhaupt <jan.weidenhaupt@oeffentliche.de>

--- core/src/main/kotlin/io/fritz2/binding/utils.kt
+++ core/src/main/kotlin/io/fritz2/binding/utils.kt
@@ -7,12 +7,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
-import kotlin.random.Random
-import kotlin.random.nextUBytes
 
 fun <T> const(value: T): Flow<T> = flowOf(value)
 
-
 fun <T> Flow<T>.watch(scope: CoroutineScope = MainScope()): Job = this.catch {}.launchIn(scope)
-
-fun uniqueId() = Random.nextUBytes(16).contentHashCode().toString()
---
+++ core/src/main/kotlin/io/fritz2/utils/uuid.kt
@@ -0,0 +1,46 @@
+package io.fritz2.utils
+
+import kotlin.js.Date
+import kotlin.random.Random
+
+fun createUUID(): String {
+    inline fun createChar(dt: Int) = (dt + Random.nextInt(16)).toString(16)  //toString(16)
+    inline fun createOther(dt: Int) = ((dt + Random.nextInt(16)) and 0x3 or 0x8).toString(16)
+
+    var dt = Date().getUTCMilliseconds();
+    val sb = StringBuilder(36)
+
+    repeat(8) {
+        sb.append(createChar(dt))
+        dt /= 16
+    }
+    sb.append('-')
+
+    repeat(4) {
+        sb.append(createChar(dt))
+        dt /= 16
+    }
+    sb.append('-', '4')
+
+    repeat(3) {
+        sb.append(createChar(dt))
+        dt /= 16
+    }
+    sb.append('-')
+
+    sb.append(createOther(dt))
+    dt /= 16
+
+    repeat(3) {
+        sb.append(createChar(dt))
+        dt /= 16
+    }
+    sb.append('-')
+
+    repeat(12) {
+        sb.append(createChar(dt))
+        dt /= 16
+    }
+
+    return sb.toString();
+}
