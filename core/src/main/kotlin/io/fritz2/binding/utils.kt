d425e947276bf46bc935615140e5ee059fbfabcc
jwstegemann <jwstegemann@gmail.com>
2020-04-08 13:38:46 +0200

    removed old id-function

--- core/src/main/kotlin/io/fritz2/binding/utils.kt
+++ core/src/main/kotlin/io/fritz2/binding/utils.kt
@@ -7,11 +7,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.launchIn
-import kotlin.random.Random
 
 fun <T> const(value: T): Flow<T> = flowOf(value)
 
-
 fun <T> Flow<T>.watch(scope: CoroutineScope = MainScope()): Job = this.catch {}.launchIn(scope)
-
-fun uniqueId() = Random.nextBytes(16).toString()
