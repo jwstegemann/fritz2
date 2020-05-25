cc7e799fcd69da435014d03befa74b888ec0bb11
Jens Stegemann <jwstegemann@gmail.com>
2020-05-25 19:55:58 +0200

    Web components (#66)
    * small dokka corrections
    
    * a first shot
    
    * working with parameter-flows
    
    * weather-card content and styling and a little convenience
    
    * github sources
    
    * better api
    
    * added lifecycle-methods
    
    * factory-method to use custom-elements in html-context
    
    * fixed wrong name
    
    * npm import of webcomponent working
    
    * test working
    
    * documentation
    
    * callback for observed attributes cannot be private
    
    Co-authored-by: Jan Weidenhaupt <jan.weidenhaupt@oeffentliche.de>

--- core/src/main/kotlin/io/fritz2/dom/tag.kt
+++ core/src/main/kotlin/io/fritz2/dom/tag.kt
@@ -24,7 +24,7 @@ annotation class HtmlTagMarker
  */
 //TODO: remove unnecassary default-arguments
 @HtmlTagMarker
-abstract class Tag<T : Element>(
+open class Tag<T : Element>(
     tagName: String,
     val id: String? = null,
     val baseClass: String? = null,
@@ -88,6 +88,8 @@ abstract class Tag<T : Element>(
             //TODO: better elvis?
             (if (baseClass != null) value.map { "$baseClass $it" } else value).bindAttr("class")
         }
+
+    var style: Flow<String> by AttributeDelegate
 
     /**
      * Delegate to bind a [Flow] of [List]s as the dynamic part of the class-attribute
