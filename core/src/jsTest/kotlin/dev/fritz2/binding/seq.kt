e311d9cc80674be6b45ddc6a95d143b564e00b4b
jwstegemann <jwstegemann@gmail.com>
2020-07-09 21:14:24 +0200

    tests working

--- core/src/jsTest/kotlin/dev/fritz2/binding/seq.kt
+++ core/src/jsTest/kotlin/dev/fritz2/binding/seq.kt
@@ -3,6 +3,7 @@ package dev.fritz2.binding
 import dev.fritz2.dom.html.render
 import dev.fritz2.dom.mount
 import dev.fritz2.identification.uniqueId
+import dev.fritz2.lenses.buildLens
 import dev.fritz2.test.initDocument
 import dev.fritz2.test.runTest
 import dev.fritz2.test.targetId
@@ -14,7 +15,7 @@ import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-class AnotherSeqTests {
+class SeqTests {
 
     private fun listContent(listId: String) = document.getElementById(listId)?.textContent
     private fun clickButton(id: String) {
@@ -31,6 +32,11 @@ class AnotherSeqTests {
     @Test
     fun testEachElement() = runTest {
         val listId = "list" + uniqueId()
+        val appendBtnId = "btn-append" + uniqueId()
+        val changeBtnId = "btn-change" + uniqueId()
+        val insertBtnId = "btn-insert" + uniqueId()
+        val deleteBtnId = "btn-delete" + uniqueId()
+
         initDocument()
 
         val store = TestListStore()
@@ -44,30 +50,30 @@ class AnotherSeqTests {
                         }
                     }.bind()
                 }
-                button(id = "append") { clicks handledBy store.append }
-                button(id = "change") { clicks handledBy store.change }
-                button(id = "insert") { clicks handledBy store.insert }
-                button(id = "delete") { clicks handledBy store.delete }
+                button(id = appendBtnId) { clicks handledBy store.append }
+                button(id = changeBtnId) { clicks handledBy store.change }
+                button(id = insertBtnId) { clicks handledBy store.insert }
+                button(id = deleteBtnId) { clicks handledBy store.delete }
             }
         }.mount(targetId)
 
         delay(200)
         assertEquals("abcd", listContent(listId), "list incorrect after init")
 
-        clickButton("append")
-        delay(200)
+        clickButton(appendBtnId)
+        delay(100)
         assertEquals("abcde", listContent(listId), "list incorrect after append")
 
-        clickButton("change")
-        delay(200)
+        clickButton(changeBtnId)
+        delay(100)
         assertEquals("axcde", listContent(listId), "list incorrect after change")
 
-        clickButton("insert")
-        delay(200)
+        clickButton(insertBtnId)
+        delay(100)
         assertEquals("yaxcde", listContent(listId), "list incorrect after insert")
 
-        clickButton("delete")
-        delay(200)
+        clickButton(deleteBtnId)
+        delay(100)
         assertEquals("yaxde", listContent(listId), "list incorrect after delete")
     }
 
@@ -76,6 +82,11 @@ class AnotherSeqTests {
     @Ignore
     fun testEachIndexStore() = runTest {
         val listId = "list" + uniqueId()
+        val appendBtnId = "btn-append" + uniqueId()
+        val changeBtnId = "btn-change" + uniqueId()
+        val insertBtnId = "btn-insert" + uniqueId()
+        val deleteBtnId = "btn-delete" + uniqueId()
+
         initDocument()
 
         val store = TestListStore()
@@ -98,24 +109,145 @@ class AnotherSeqTests {
             }
         }.mount(targetId)
 
-        delay(500)
-//        println("####### " + document.getElementById("hugo")?.textContent)
-//        assertEquals("abcd", listContent(listId),"list incorrect after init")
+        delay(200)
+        assertEquals("abcd", listContent(listId), "list incorrect after init")
 
-        clickButton("append")
-        delay(500)
+        clickButton(appendBtnId)
+        delay(100)
         assertEquals("abcde", listContent(listId), "list incorrect after append")
 
         clickButton("change")
-        delay(200)
+        delay(100)
         assertEquals("axcde", listContent(listId), "list incorrect after change")
 
         clickButton("insert")
-        delay(200)
+        delay(100)
         assertEquals("yaxcde", listContent(listId), "list incorrect after insert")
 
         clickButton("delete")
-        delay(200)
+        delay(100)
         assertEquals("yaxde", listContent(listId), "list incorrect after delete")
+    }
+
+
+/*
+     * with id...
+     */
+
+    data class Entity(val id: String, val value: String)
+
+    val valueLens = buildLens("value", Entity::value, { p, v -> p.copy(value = v) })
+
+    class TestEntityListStore() : RootStore<List<Entity>>(
+        listOf(
+            Entity("1", "a"),
+            Entity("2", "b"),
+            Entity("3", "c"),
+            Entity("4", "d")
+        )
+    ) {
+        val append = handle { model -> model + Entity("5", "e") }
+        val change = handle { model -> listOf(model.first(), Entity("2", "x")) + model.takeLast(3) }
+        val insert = handle { model -> listOf(Entity("0", "y")) + model }
+        val delete = handle { model -> model.take(3) + model.takeLast(2) }
+    }
+
+    @Test
+    fun testEachEntity() = runTest {
+        val listId = "list" + uniqueId()
+        val appendBtnId = "btn-append" + uniqueId()
+        val changeBtnId = "btn-change" + uniqueId()
+        val insertBtnId = "btn-insert" + uniqueId()
+        val deleteBtnId = "btn-delete" + uniqueId()
+
+        initDocument()
+
+        val store = TestEntityListStore()
+
+        render {
+            div {
+                ul(id = listId) {
+                    store.data.each(Entity::id).map {
+                        render {
+                            li { text(it.value) }
+                        }
+                    }.bind()
+                }
+                button(id = appendBtnId) { clicks handledBy store.append }
+                button(id = changeBtnId) { clicks handledBy store.change }
+                button(id = insertBtnId) { clicks handledBy store.insert }
+                button(id = deleteBtnId) { clicks handledBy store.delete }
+            }
+        }.mount(targetId)
+
+        delay(200)
+        assertEquals("abcd", listContent(listId), "list incorrect after init")
+
+        clickButton(appendBtnId)
+        delay(100)
+        assertEquals("abcde", listContent(listId), "list incorrect after append")
+
+        clickButton(changeBtnId)
+        delay(100)
+        assertEquals("abcde", listContent(listId), "list incorrect after change")
+
+        clickButton(insertBtnId)
+        delay(100)
+        assertEquals("yabcde", listContent(listId), "list incorrect after insert")
+
+        clickButton(deleteBtnId)
+        delay(100)
+        assertEquals("yabde", listContent(listId), "list incorrect after delete")
+    }
+
+
+    @Test
+    fun testEachEntityStore() = runTest {
+        val listId = "list" + uniqueId()
+        val appendBtnId = "btn-append" + uniqueId()
+        val changeBtnId = "btn-change" + uniqueId()
+        val insertBtnId = "btn-insert" + uniqueId()
+        val deleteBtnId = "btn-delete" + uniqueId()
+
+        initDocument()
+
+        val store = TestEntityListStore()
+
+        render {
+            div {
+                ul(id = listId) {
+                    store.each(Entity::id).map {
+                        val valueStore = it.sub(valueLens)
+                        render {
+                            li { valueStore.data.bind() }
+                        }
+                    }.bind()
+                }
+                button(id = appendBtnId) { clicks handledBy store.append }
+                button(id = changeBtnId) { clicks handledBy store.change }
+                button(id = insertBtnId) { clicks handledBy store.insert }
+                button(id = deleteBtnId) { clicks handledBy store.delete }
+            }
+        }.mount(targetId)
+
+        delay(200)
+        assertEquals("abcd", listContent(listId), "list incorrect after init")
+
+        clickButton(appendBtnId)
+        delay(100)
+        assertEquals("abcde", listContent(listId), "list incorrect after append")
+
+        clickButton(changeBtnId)
+        delay(100)
+        assertEquals("axcde", listContent(listId), "list incorrect after change")
+
+        clickButton(insertBtnId)
+        delay(100)
+        assertEquals("yaxcde", listContent(listId), "list incorrect after insert")
+
+        clickButton(deleteBtnId)
+        delay(100)
+        assertEquals("yaxde", listContent(listId), "list incorrect after delete")
+
     }
 }
