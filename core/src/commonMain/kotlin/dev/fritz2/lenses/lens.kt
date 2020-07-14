d32ace72388fbb885f2f88c1d09e08499d17c9d6
jamowei <jan@rexster.de>
2020-07-14 23:54:50 +0200

    put lenses code together

--- core/src/commonMain/kotlin/dev/fritz2/lenses/lens.kt
+++ core/src/commonMain/kotlin/dev/fritz2/lenses/lens.kt
@@ -21,3 +21,41 @@ inline fun <P, T> buildLens(id: String, crossinline getter: (P) -> T, crossinlin
     override fun get(parent: P): T = getter(parent)
     override fun set(parent: P, value: T): P = setter(parent, value)
 }
+
+/**
+ * function to derive a valid id for a given instance that does not change over time.
+ */
+typealias idProvider<T> = (T) -> String
+
+/**
+ * creates a [Lens] pointing to a certain element in a list
+ *
+ * @param element current instance of the element to focus on
+ * @param id [idProvider] to identify the element in the list (i.e. when it's content changes over time)
+ */
+fun <T> elementLens(element: T, id: idProvider<T>): Lens<List<T>, T> = object :
+    Lens<List<T>, T> {
+    override val _id: String = id(element)
+
+    override fun get(parent: List<T>): T = parent.find {
+        id(it) == id(element)
+    } ?: throw IndexOutOfBoundsException()
+
+    override fun set(parent: List<T>, value: T): List<T> = parent.map {
+        if (id(it) == id(value)) value else it
+    }
+}
+
+/**
+ * creates a [Lens] pointing to a certain position in a list
+ *
+ * @param index position to focus on
+ */
+fun <T> positionLens(index: Int): Lens<List<T>, T> = object : Lens<List<T>, T> {
+    override val _id: String = index.toString()
+
+    override fun get(parent: List<T>): T = parent[index]
+
+    override fun set(parent: List<T>, value: T): List<T> =
+        parent.subList(0, index) + value + parent.subList(index + 1, parent.size)
+}
