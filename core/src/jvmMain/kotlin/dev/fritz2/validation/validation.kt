27789ff9dbe9e29c90fe1fe7b853bb0f124f8050
jamowei <jan@rexster.de>
2020-07-24 15:30:59 +0200

    added isValid function for jvm side

--- core/src/jvmMain/kotlin/dev/fritz2/validation/validation.kt
+++ core/src/jvmMain/kotlin/dev/fritz2/validation/validation.kt
@@ -4,5 +4,11 @@ package dev.fritz2.validation
  * Implement this interface to describe, how a certain data-model should be validated.
  */
 actual abstract class Validator<D, M : ValidationMessage, T> actual constructor() {
+
     actual abstract fun validate(data: D, metadata: T): List<M>
+
+    /**
+     * A [Boolean] representing the current state of the model (valid or not).
+     */
+    fun isValid(data: D, metadata: T): Boolean = validate(data, metadata).none(ValidationMessage::failed)
 }
