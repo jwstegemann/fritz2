5b71a680cc86090ca9d6541f910c92ef40428c76
jamowei <jan@rexster.de>
2020-08-03 19:33:43 +0200

    added tests

---
+++ core/src/commonTest/kotlin/dev/fritz2/validation/validation.kt
@@ -0,0 +1,51 @@
+package dev.fritz2.validation
+
+import dev.fritz2.lenses.Lenses
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+val carNameIsBlank = ValidationTests.Message("car name can not be blank")
+val colorValuesAreToLow = ValidationTests.Message("color members are lower then 0")
+val colorValuesAreToHigh = ValidationTests.Message("color members are greater then 255")
+
+val carValidator = validator { car: ValidationTests.Car, _: Unit ->
+    val msgs = mutableListOf<ValidationTests.Message>()
+
+    if(car.name.isBlank())
+        msgs.add(carNameIsBlank)
+
+    if(car.color.r < 0 || car.color.g < 0 || car.color.b < 0)
+        msgs.add(colorValuesAreToLow)
+
+    if(car.color.r > 255 || car.color.g > 255 || car.color.b > 255)
+        msgs.add(colorValuesAreToHigh)
+
+    msgs
+}
+
+class ValidationTests {
+
+    @Lenses
+    data class Car(val name: String, val color: Color)
+
+    @Lenses
+    data class Color(val r: Int, val g: Int, val b: Int)
+
+    class Message(val text: String) : ValidationMessage {
+        override fun failed(): Boolean = true
+
+    }
+
+    @Test
+    fun testValidation() {
+        val c1 = Car(" ", Color(120, 120, 120))
+        val c2 = Car("car1", Color(-1, -1, -1))
+        val c3 = Car("car2", Color(256, 256, 256))
+        val c4 = Car(" ", Color(256, -1, 120))
+
+        assertEquals(carNameIsBlank, carValidator.validate(c1, Unit).first())
+        assertEquals(colorValuesAreToLow, carValidator.validate(c2, Unit).first())
+        assertEquals(colorValuesAreToHigh, carValidator.validate(c3, Unit).first())
+        assertEquals(3, carValidator.validate(c4, Unit).size, "number of messages not right")
+    }
+}
---
+++ core/src/jsTest/kotlin/dev/fritz2/validation/validation.kt
@@ -0,0 +1,91 @@
+package dev.fritz2.validation
+
+import dev.fritz2.binding.*
+import dev.fritz2.dom.html.render
+import dev.fritz2.dom.mount
+import dev.fritz2.identification.uniqueId
+import dev.fritz2.test.initDocument
+import dev.fritz2.test.runTest
+import dev.fritz2.test.targetId
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.map
+import org.w3c.dom.HTMLDivElement
+import kotlin.browser.document
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ValidationJSTests {
+
+    @Test
+    fun testValidation() = runTest {
+        initDocument()
+
+        val carName = "ok"
+        val c1 = ValidationTests.Car(" ", ValidationTests.Color(120, 120, 120))
+        val c2 = ValidationTests.Car("car1", ValidationTests.Color(-1, -1, -1))
+        val c3 = ValidationTests.Car("car2", ValidationTests.Color(256, 256, 256))
+        val c4 = ValidationTests.Car(" ", ValidationTests.Color(256, -1, 120))
+
+        val store =
+            object : RootStore<ValidationTests.Car>(
+                ValidationTests.Car(carName, ValidationTests.Color(120, 120, 120))
+            ) {
+                override val update = handle<ValidationTests.Car> { old, new ->
+                    if (carValidator.isValid(new, Unit)) new else old
+                }
+            }
+
+        val idData = "data-${uniqueId()}"
+        val idMessages = "messages-${uniqueId()}"
+
+        render {
+            div {
+                div(id = idData) {
+                    store.data.map { it.name }.bind()
+                }
+                div(id = idMessages) {
+                    carValidator.msgs.each(ValidationTests.Message::text).render {
+                        p {
+                            +it.text
+                        }
+                    }.bind()
+                }
+            }
+        }.mount(targetId)
+
+        delay(100)
+        val divData = document.getElementById(idData) as HTMLDivElement
+        val divMessages = document.getElementById(idMessages) as HTMLDivElement
+
+        assertEquals(carName, divData.textContent, "initial car name is wrong")
+        assertEquals(0, divMessages.childElementCount, "there are messages")
+
+        action(c1) handledBy store.update
+        delay(100)
+
+        assertEquals(carName, divData.innerText, "c1: car name has changed")
+        assertEquals(1, divMessages.childElementCount, "c1: there is not 1 message")
+        assertEquals(carNameIsBlank.text, divMessages.firstElementChild?.textContent, "c1: there is not expected message")
+
+        action(c2) handledBy store.update
+        delay(100)
+
+        assertEquals(carName, divData.innerText, "c2: car name has changed")
+        assertEquals(1, divMessages.childElementCount, "c2: there is not 1 message")
+        assertEquals(colorValuesAreToLow.text, divMessages.firstElementChild?.textContent, "c2: there is not expected message")
+
+        action(c3) handledBy store.update
+        delay(100)
+
+        assertEquals(carName, divData.innerText, "c3: car name has changed")
+        assertEquals(1, divMessages.childElementCount, "c3: there is not 1 message")
+        assertEquals(colorValuesAreToHigh.text, divMessages.firstElementChild?.textContent, "c3: there is not expected message")
+
+        action(c4) handledBy store.update
+        delay(100)
+
+        assertEquals(carName, divData.innerText, "c4: car name has changed")
+        assertEquals(3, divMessages.childElementCount, "c4: there is not 3 message")
+    }
+
+}
