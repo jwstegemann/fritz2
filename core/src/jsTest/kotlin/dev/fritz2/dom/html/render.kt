31182c89c3ad3fce695e8b50a0a187d17ecfa38f
jamowei <jan@rexster.de>
2020-08-11 11:05:52 +0200

    added test

---
+++ core/src/jsTest/kotlin/dev/fritz2/dom/html/render.kt
@@ -0,0 +1,50 @@
+package dev.fritz2.dom.html
+
+import dev.fritz2.binding.action
+import dev.fritz2.binding.handledBy
+import dev.fritz2.binding.storeOf
+import dev.fritz2.dom.mount
+import dev.fritz2.identification.uniqueId
+import dev.fritz2.test.initDocument
+import dev.fritz2.test.runTest
+import dev.fritz2.test.targetId
+import kotlinx.coroutines.delay
+import org.w3c.dom.HTMLDivElement
+import kotlin.browser.document
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class RenderTests {
+
+    @Test
+    fun testShortRenderFunction() = runTest {
+        initDocument()
+
+        val store = storeOf(true)
+
+        val divId = uniqueId()
+
+        render {
+            section {
+                store.data.render { value ->
+                    div(id = divId) {
+                        +if (value) "on" else "off"
+                    }
+                }.bind()
+            }
+        }.mount(targetId)
+
+        delay(100)
+
+        val div = document.getElementById(divId) as HTMLDivElement
+
+        assertEquals("on", div.textContent)
+
+        action(false) handledBy store.update
+        delay(200)
+
+        val div2 = document.getElementById(divId) as HTMLDivElement
+
+        assertEquals("off", div2.textContent)
+    }
+}
