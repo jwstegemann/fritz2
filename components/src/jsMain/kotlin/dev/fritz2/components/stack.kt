95653e674ca2ca6e7e70b2febbfc4b1fb0c3c51f
jwstegemann <jwstegemann@gmail.com>
2020-10-08 17:25:04 +0200

    started stack

--- components/src/jsMain/kotlin/dev/fritz2/components/stack.kt
+++ components/src/jsMain/kotlin/dev/fritz2/components/stack.kt
@@ -1,14 +1,11 @@
 package dev.fritz2.components
 
-import dev.fritz2.dom.html.HtmlElements
-import dev.fritz2.styling.params.FlexStyleParams
-import dev.fritz2.styling.params.Style
-
+/*
 fun HtmlElements.Stack(styles: Style<FlexStyleParams>) {
 
 //    val stackStyles = {}
 
-/*    return flex(stackStyles + styles) {
+    return flex(stackStyles + styles) {
 
     }
 */
