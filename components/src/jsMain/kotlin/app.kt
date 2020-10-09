a8f52ed979ae1cbe4c745191a6e247a5fcd87a18
jwstegemann <jwstegemann@gmail.com>
2020-10-09 11:18:08 +0200

    rename StackUp and Lineup

--- components/src/jsMain/kotlin/app.kt
+++ components/src/jsMain/kotlin/app.kt
@@ -144,7 +144,7 @@ fun HtmlElements.flexDemo(theme: ExtendedTheme): Div {
                             +"Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers."
                         }
                     }
-                    Group {
+                    LineUp {
                         Button() {
                             Icon(theme.icons.arrowUp)
                             +"Normal"
