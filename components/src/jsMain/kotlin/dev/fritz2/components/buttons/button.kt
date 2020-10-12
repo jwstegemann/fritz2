7d7ba3d6dab50dadb051f979bf8a2d7123bd6cb9
jwstegemann <jwstegemann@gmail.com>
2020-10-12 12:48:51 +0200

    cleanup (work in progress)

---
+++ components/src/jsMain/kotlin/dev/fritz2/components/buttons/button.kt
@@ -0,0 +1,235 @@
+package dev.fritz2.components.buttons
+
+import dev.fritz2.components.Spinner
+import dev.fritz2.components.hidden
+import dev.fritz2.dom.html.Button
+import dev.fritz2.dom.html.HtmlElements
+import dev.fritz2.dom.html.renderAll
+import dev.fritz2.styling.params.*
+import dev.fritz2.styling.staticStyle
+import dev.fritz2.styling.theme.theme
+import kotlinx.coroutines.flow.Flow
+
+
+internal object ButtonFoundation {
+    val css = staticStyle(
+        "button",
+        """
+        appearance: none;
+        display: -flex;
+        align-items : center;
+        justify-content: center;
+        transition: all 250ms;
+        user-select: none;
+        position: relative;
+        white-space: nowrap;
+        vertical-align: middle;
+        outline: none;
+        
+        &:disabled {
+            opacity: 0.4;
+            cursor: not-allowed;
+            boxShadow: none;
+        }
+    """
+    )
+
+    fun color(value: ColorProperty): Style<BasicStyleParams> = {
+        css("--main-color: $value;")
+    }
+
+    private const val iconSize = "1.15em"
+    private const val marginToText = "0.35rem"
+    private const val marginToBorder = "-0.2rem"
+
+    val centerIconStyle: Style<BasicStyleParams> = {
+        width { iconSize }
+        height { iconSize }
+    }
+
+    val leftIconStyle: Style<BasicStyleParams> = {
+        width { iconSize }
+        height { iconSize }
+        margins {
+            left { marginToBorder }
+            right { marginToText }
+        }
+    }
+
+    val rightIconStyle: Style<BasicStyleParams> = {
+        width { iconSize }
+        height { iconSize }
+        margins {
+            right { marginToBorder }
+            left { marginToText }
+        }
+    }
+}
+
+
+/*
+fun HtmlElements.Button(
+    text: String,
+    styles: Style<BasicStyleParams> = {},
+    color: ColorProperty = theme().colors.primary,
+    variant: ButtonVariants.() -> Style<BasicStyleParams> = { solid },
+    size: ButtonSizes.() -> Style<BasicStyleParams> = { normal }
+): Flow<Unit> {
+    lateinit var buttonClicks: Flow<Unit>
+    Button(styles, color, variant, size) {
+        buttonClicks = clicks.map { Unit }
+        +text
+    }
+    return buttonClicks
+}
+
+val hidden = staticStyle(
+    "hidden", """
+    visibility: hidden;
+"""
+)
+
+fun HtmlElements.Button(
+    text: String,
+    loading: Flow<Boolean>,
+    styles: Style<BasicStyleParams> = {},
+    loadingText: String? = null,
+    color: ColorProperty = theme().colors.primary,
+    variant: ButtonVariants.() -> Style<BasicStyleParams> = { solid },
+    size: ButtonSizes.() -> Style<BasicStyleParams> = { normal }
+): Flow<Unit> {
+    lateinit var buttonClicks: Flow<Unit>
+    Button(styles, color, variant, size) {
+        buttonClicks = clicks.map { Unit }
+        }.bind()
+    }
+    return buttonClicks
+}
+
+
+
+ fun HtmlElements.Button(
+    text: String,
+    icon: IconDefinition,
+    loading: Flow<Boolean>,
+    styles: Style<BasicStyleParams> = {},
+    loadingText: String? = null,
+    iconRight: Boolean = false,
+    color: ColorProperty = theme().colors.primary,
+    variant: ButtonVariants.() -> Style<BasicStyleParams> = { solid },
+    size: ButtonSizes.() -> Style<BasicStyleParams> = { normal }
+): Flow<Unit> {
+    lateinit var buttonClicks: Flow<Unit>
+    Button(styles, color, variant, size) {
+        buttonClicks = clicks.map { Unit }
+        loading.renderAll { running ->
+            if (!iconRight) {
+                if (running) {
+                    Spinner({
+                        buttonLeftIconStyle()
+                    })
+                } else Icon(icon, buttonLeftIconStyle)
+            }
+            span { +(if (running && loadingText != null) loadingText else text) }
+            if (iconRight) {
+                if (running) {
+                    Spinner({
+                        buttonRightIconStyle()
+                    })
+                } else Icon(icon, buttonRightIconStyle)
+            }
+        }.bind()
+    }
+    return buttonClicks
+}
+*/
+
+fun HtmlElements.Button(
+    styles: Style<BasicStyleParams> = {},
+    color: ColorProperty = theme().colors.primary,
+    variant: ButtonVariants.() -> Style<BasicStyleParams> = { solid },
+    size: ButtonSizes.() -> Style<BasicStyleParams> = { normal },
+    init: Button.() -> Any
+): Button {
+    return button(
+        "${ButtonFoundation}.css ${
+            use(
+                ButtonSizes.size() +
+                        ButtonFoundation.color(color) +
+                        ButtonVariants.variant() +
+                        styles,
+                "button"
+            )
+        }"
+    ) {
+        init()
+    }
+}
+
+fun HtmlElements.ClickButton(
+    styles: Style<BasicStyleParams> = {},
+    color: ColorProperty = theme().colors.primary,
+    variant: ButtonVariants.() -> Style<BasicStyleParams> = { solid },
+    size: ButtonSizes.() -> Style<BasicStyleParams> = { normal },
+    init: Button.() -> Any
+): Flow<Unit> {
+    lateinit var buttonClicks: Flow<Unit>
+    Button(styles, color, variant, size) {
+        buttonClicks = clicks.map {}
+        init()
+    }
+    return buttonClicks
+}
+
+
+internal fun Button.label(text: String) {
+    span { +text }
+}
+
+internal fun Button.label(text: Flow<String>) {
+    span { text.bind() }
+}
+
+internal fun Button.label(text: String, loading: Flow<Boolean>, loadingText: String?) {
+    loading.renderAll { running ->
+        //render spinner
+        if (running) {
+            Spinner({
+                if (loadingText == null) css("position: absolute;")
+                else ButtonFoundation.leftIconStyle()
+            })
+            span(if (loadingText == null) hidden else "") { +(loadingText ?: text) }
+        } else {
+            span { +text }
+        }
+    }.bind()
+}
+
+
+fun HtmlElements.Button(
+    text: String,
+    styles: Style<BasicStyleParams> = {},
+    color: ColorProperty = theme().colors.primary,
+    variant: ButtonVariants.() -> Style<BasicStyleParams>,
+    size: ButtonSizes.() -> Style<BasicStyleParams>,
+): Flow<Unit> {
+    return ClickButton(styles, color, variant, size) {
+        label(text)
+    }
+}
+
+fun HtmlElements.Button(
+    text: String,
+    loading: Flow<Boolean>,
+    loadingText: String? = null,
+    styles: Style<BasicStyleParams> = {},
+    color: ColorProperty = theme().colors.primary,
+    variant: ButtonVariants.() -> Style<BasicStyleParams>,
+    size: ButtonSizes.() -> Style<BasicStyleParams>,
+): Flow<Unit> {
+    return ClickButton(styles, color, variant, size) {
+        label(text, loading, loadingText)
+    }
+}
+
+
---
+++ components/src/jsMain/kotlin/dev/fritz2/components/utilities.kt
@@ -0,0 +1,10 @@
+package dev.fritz2.components
+
+import dev.fritz2.styling.staticStyle
+
+internal val hidden = staticStyle(
+    "hidden", """
+    visibility: hidden;
+"""
+)
+
