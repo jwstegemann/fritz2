d8f451374f391425bbaefbe91f21830025dfc635
chausknecht <hausknecht@nundp.com>
2020-10-26 16:11:36 +0100

    Add ``themeProvider`` top level component
    - themeProvider manages the theme or multiple themes
    - it offers a special store interface to react to theme changes or to actively set the theme (example in the flexBoxDemo via a selectBox)
    - it sets default styles for basic text elements
    - it offers an option to *reset* all default browser styles
    - minor enhancements for some demos

--- components/src/jsMain/kotlin/app.kt
+++ components/src/jsMain/kotlin/app.kt
@@ -1,6 +1,8 @@
 import dev.fritz2.binding.const
+import dev.fritz2.binding.handledBy
 import dev.fritz2.components.flexBox
 import dev.fritz2.components.styled
+import dev.fritz2.components.themeProvider
 import dev.fritz2.dom.html.A
 import dev.fritz2.dom.html.HtmlElements
 import dev.fritz2.dom.html.render
@@ -11,11 +13,15 @@ import dev.fritz2.styling.params.BasicParams
 import dev.fritz2.styling.theme.currentTheme
 import dev.fritz2.styling.theme.render
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
 
+/*
 val themes = listOf<Pair<String, ExtendedTheme>>(
     ("small Fonts") to SmallFonts(),
     ("large Fonts") to LargeFonts()
 )
+
+ */
 
 
 fun HtmlElements.myLink(
@@ -58,68 +64,83 @@ fun HtmlElements.myBorderedRedLink(
 
 @ExperimentalCoroutinesApi
 fun main() {
-    currentTheme = themes.first().second
+    //currentTheme = themes.first().second
+
+    val themes = listOf<Pair<String, ExtendedTheme>>(
+        ("small Fonts") to SmallFonts(),
+        ("large Fonts") to LargeFonts()
+    )
 
     val router = router("")
 
     render { theme: ExtendedTheme ->
-        section {
-            flexBox({
-                height { "60px" }
-                wrap { nowrap }
-                direction { row }
-                justifyContent { spaceEvenly }
-                alignItems { center }
-            }) {
-                (::a.styled {
-                    fontSize { large }
-                }) {
-                    +"text"
-                    href = const("#text")
-                }
-                (::a.styled {
-                    fontSize { large }
-                }) {
-                    +"flexBox"
-                    href = const("#flexBoxDemo")
-                }
-                (::a.styled {
-                    fontSize { large }
-                }) {
-                    +"gridBox"
-                    href = const("#gridBoxDemo")
-                }
-                (::a.styled {
-                    fontSize { large }
-                }) {
-                    +"input"
-                    href = const("#input")
-                }
-                (::a.styled {
-                    fontSize { large }
-                }) {
-                    +"formcontrol"
-                    href = const("#formcontrol")
-                }
-                (::a.styled {
-                    fontSize { large }
-                }) {
-                    +"stack"
-                    href = const("#stack")
+        themeProvider {
+            // grab the ThemeStore to pass it down towards components, that need to access either the
+            // data or to change the selected theme
+            val store = themeStore
+            // could be the default? (So only need to set it to ``false`` actively?)
+            resetCss { true }
+            // Override default theme by a new list of themes
+            themes { themes }
+            items {
+                section {
+                    flexBox({
+                        height { "60px" }
+                        wrap { nowrap }
+                        direction { row }
+                        justifyContent { spaceEvenly }
+                        alignItems { center }
+                    }) {
+                        (::a.styled {
+                            fontSize { large }
+                        }) {
+                            +"text"
+                            href = const("#text")
+                        }
+                        (::a.styled {
+                            fontSize { large }
+                        }) {
+                            +"flexBox"
+                            href = const("#flexBoxDemo")
+                        }
+                        (::a.styled {
+                            fontSize { large }
+                        }) {
+                            +"gridBox"
+                            href = const("#gridBoxDemo")
+                        }
+                        (::a.styled {
+                            fontSize { large }
+                        }) {
+                            +"input"
+                            href = const("#input")
+                        }
+                        (::a.styled {
+                            fontSize { large }
+                        }) {
+                            +"formcontrol"
+                            href = const("#formcontrol")
+                        }
+                        (::a.styled {
+                            fontSize { large }
+                        }) {
+                            +"stack"
+                            href = const("#stack")
+                        }
+                    }
+                    router.render { site ->
+                        when (site) {
+                            "input" -> inputDemo()
+//                    "buttons" -> buttonDemo(theme)
+                            "formcontrol" -> formControlDemo()
+                            "text" -> textDemo()
+                            "flexBoxDemo" -> flexBoxDemo(store, themes)
+                            "gridBoxDemo" -> gridBoxDemo()
+                            else -> stackDemo(theme)
+                        }
+                    }.bind()
                 }
             }
-            router.render { site ->
-                when (site) {
-//                    "grid" -> gridDemo()
-                    "input" -> inputDemo()
-//                    "buttons" -> buttonDemo(theme)
-                    "formcontrol" -> formControlDemo()
-                    "text" -> textDemo()
-                    "flexBoxDemo" -> flexBoxDemo(theme)
-                    "gridBoxDemo" -> gridBoxDemo()
-                    else -> stackDemo(theme)
-                }
-            }.bind()
         }
     }.mount("target")
 }
