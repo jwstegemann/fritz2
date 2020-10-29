75168a672e967dfd0b0c4f5bba0133638d589449
jwstegemann <jwstegemann@gmail.com>
2020-10-29 08:24:06 +0100

    started buttons rework

--- components/src/jsMain/kotlin/buttonDemo.kt
+++ components/src/jsMain/kotlin/buttonDemo.kt
@@ -1,4 +1,13 @@
-/*
+import dev.fritz2.binding.RootStore
+import dev.fritz2.binding.watch
+import dev.fritz2.components.flexBox
+import dev.fritz2.dom.html.Div
+import dev.fritz2.dom.html.HtmlElements
+import dev.fritz2.tracking.tracker
+import kotlinx.browser.window
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+
 @ExperimentalCoroutinesApi
 fun HtmlElements.buttonDemo(theme: ExtendedTheme): Div {
 
@@ -18,37 +27,34 @@ fun HtmlElements.buttonDemo(theme: ExtendedTheme): Div {
     buttonStore.watch()
 
     return div {
-        f2Flex {
+        flexBox({
             direction { column }
             padding { normal }
-        }.apply {
+        }) {
             h1 { +"Button Showcase" }
 
-            f2StackUp().apply {
-                f2LineUp().apply {
-                    ClickButton("save") handledBy buttonStore.showMsg
-                    ClickButton("save", buttonStore.loading) handledBy buttonStore.showMsg
-                    ClickButton("save", buttonStore.loading, "saving...") handledBy buttonStore.showMsg
-                }
+//            f2StackUp().apply {
+//                f2LineUp().apply {
+//                    ClickButton("save") handledBy buttonStore.showMsg
+//                    ClickButton("save", buttonStore.loading) handledBy buttonStore.showMsg
+//                    ClickButton("save", buttonStore.loading, "saving...") handledBy buttonStore.showMsg
+//                }
+//
+//                f2LineUp().apply {
+//                    ClickButton(theme.icons.arrowUp) handledBy buttonStore.showMsg
+//                    ClickButton(theme.icons.arrowUp, buttonStore.loading) handledBy buttonStore.showMsg
+//                }
+//
+//                f2LineUp().apply {
+//                    ClickButton(theme.icons.arrowUp, "save") handledBy buttonStore.showMsg
+//                    ClickButton(theme.icons.arrowUp, "save", buttonStore.loading) handledBy buttonStore.showMsg
+//                }
+//
+//                f2LineUp().apply {
+//                    ClickButton("save", theme.icons.arrowUp) handledBy buttonStore.showMsg
+//                    ClickButton("save", theme.icons.arrowUp, buttonStore.loading) handledBy buttonStore.showMsg
+//                }
 
-                f2LineUp().apply {
-                    ClickButton(theme.icons.arrowUp) handledBy buttonStore.showMsg
-                    ClickButton(theme.icons.arrowUp, buttonStore.loading) handledBy buttonStore.showMsg
-                }
-
-                f2LineUp().apply {
-                    ClickButton(theme.icons.arrowUp, "save") handledBy buttonStore.showMsg
-                    ClickButton(theme.icons.arrowUp, "save", buttonStore.loading) handledBy buttonStore.showMsg
-                }
-
-                f2LineUp().apply {
-                    ClickButton("save", theme.icons.arrowUp) handledBy buttonStore.showMsg
-                    ClickButton("save", theme.icons.arrowUp, buttonStore.loading) handledBy buttonStore.showMsg
-                }
-
-            }
         }
     }
 }
-
- */
