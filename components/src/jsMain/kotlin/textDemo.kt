dac90d5aa2ad65c5a95e5bd696a12d934a2104a2
chausknecht <hausknecht@nundp.com>
2020-11-03 18:26:00 +0100

    Improve initial styling
    - added some properties based upon the theme like font-sizes
    - added default list styles (disc and numeric)

--- components/src/jsMain/kotlin/textDemo.kt
+++ components/src/jsMain/kotlin/textDemo.kt
@@ -1,3 +1,4 @@
+import dev.fritz2.binding.const
 import dev.fritz2.components.*
 import dev.fritz2.dom.html.Div
 import dev.fritz2.dom.html.HtmlElements
@@ -28,11 +29,18 @@ fun HtmlElements.textDemo(): Div {
                     }) { +" Span is integrated into a container like a <p> as in this case. " }
                     +" Expedita sit praesentium dolores obcaecati possimus sapiente voluptatem doloribus, ipsum harum in quia, provident corporis nulla corrupti placeat!"
                 }
+                p {
+                    +"Hier folgt ein "
+                    a {
+                        +"Link"
+                        const("#nowhere")
+                    }
+                }
                 ul {
-                    (1 .. 5).map { li { +"Item $it" } }
+                    (1..5).map { li { +"Item $it" } }
                 }
                 ol {
-                    (1 .. 5).map { li { +"Item $it" } }
+                    (1..5).map { li { +"Item $it" } }
                 }
             }
         }
