31aef786786d722617d591ea5222df6b7bcea508
Jan Weidenhaupt <jan.weidenhaupt@oeffentliche.de>
2021-02-22 14:37:39 +0100

    added kdoc

--- core/src/jsMain/kotlin/dev/fritz2/lenses/lens.kt
+++ core/src/jsMain/kotlin/dev/fritz2/lenses/lens.kt
@@ -1,47 +1,92 @@
 package dev.fritz2.lenses
 
+/**
+ * Creates a [Lens] from [Byte] to [String]
+ */
 fun <P> Lens<P, Byte>.asString(): Lens<P, String> =
     this + format(String::toByte, Byte::toString)
 
+/**
+ * Creates a [Lens] from [Byte] to [String]
+ */
 fun <P> Lens<P, Byte?>.asString(emptyValue: String = ""): Lens<P, String> =
     this + format({ if (it.isBlank()) null else it.toByte() }, { it?.toString() ?: emptyValue })
 
+/**
+ * Creates a [Lens] from [Short] to [String]
+ */
 fun <P> Lens<P, Short>.asString(): Lens<P, String> =
     this + format(String::toShort, Short::toString)
 
+/**
+ * Creates a [Lens] from [Short] to [String]
+ */
 fun <P> Lens<P, Short?>.asString(emptyValue: String = ""): Lens<P, String> =
     this + format({ if (it.isBlank()) null else it.toShort() }, { it?.toString() ?: emptyValue })
 
+/**
+ * Creates a [Lens] from [Int] to [String]
+ */
 fun <P> Lens<P, Int>.asString(): Lens<P, String> =
     this + format(String::toInt, Int::toString)
 
+/**
+ * Creates a [Lens] from [Int] to [String]
+ */
 fun <P> Lens<P, Int?>.asString(emptyValue: String = ""): Lens<P, String> =
     this + format({ if (it.isBlank()) null else it.toInt() }, { it?.toString() ?: emptyValue })
 
+/**
+ * Creates a [Lens] from [Long] to [String]
+ */
 fun <P> Lens<P, Long>.asString(): Lens<P, String> =
     this + format(String::toLong, Long::toString)
 
+/**
+ * Creates a [Lens] from [Long] to [String]
+ */
 fun <P> Lens<P, Long?>.asString(emptyValue: String = ""): Lens<P, String> =
     this + format({ if (it.isBlank()) null else it.toLong() }, { it?.toString() ?: emptyValue })
 
+/**
+ * Creates a [Lens] from [Double] to [String]
+ */
 fun <P> Lens<P, Double>.asString(): Lens<P, String> =
     this + format(String::toDouble, Double::toString)
 
+/**
+ * Creates a [Lens] from [Double] to [String]
+ */
 fun <P> Lens<P, Double?>.asString(emptyValue: String = ""): Lens<P, String> =
     this + format({ if (it.isBlank()) null else it.toDouble() }, { it?.toString() ?: emptyValue })
 
+/**
+ * Creates a [Lens] from [Float] to [String]
+ */
 fun <P> Lens<P, Float>.asString(): Lens<P, String> =
     this + format(String::toFloat, Float::toString)
 
+/**
+ * Creates a [Lens] from [Float] to [String]
+ */
 fun <P> Lens<P, Float?>.asString(emptyValue: String = ""): Lens<P, String> =
     this + format({ if (it.isBlank()) null else it.toFloat() }, { it?.toString() ?: emptyValue })
 
+/**
+ * Creates a [Lens] from [Boolean] to [String]
+ */
 fun <P> Lens<P, Boolean>.asString(): Lens<P, String> =
     this + format(String::toBoolean, Boolean::toString)
 
+/**
+ * Creates a [Lens] from [Boolean] to [String]
+ */
 fun <P> Lens<P, Boolean?>.asString(emptyValue: String = ""): Lens<P, String> =
     this + format({ if (it.isBlank()) null else it.toBoolean() }, { it?.toString() ?: emptyValue })
 
+/**
+ * Creates a [Lens] from [List] of [Byte] to [String]
+ */
 fun <P> Lens<P, List<Byte>>.asString(
     separator: String = ", ",
     prefix: String = "",
@@ -52,6 +97,9 @@ fun <P> Lens<P, List<Byte>>.asString(
         { it.joinToString(separator, prefix, postfix) }
     )
 
+/**
+ * Creates a [Lens] from [List] of [Short] to [String]
+ */
 fun <P> Lens<P, List<Short>>.asString(
     separator: String = ", ",
     prefix: String = "",
@@ -62,6 +110,9 @@ fun <P> Lens<P, List<Short>>.asString(
         { it.joinToString(separator, prefix, postfix) }
     )
 
+/**
+ * Creates a [Lens] from [List] of [Int] to [String]
+ */
 fun <P> Lens<P, List<Int>>.asString(
     separator: String = ", ",
     prefix: String = "",
@@ -72,6 +123,9 @@ fun <P> Lens<P, List<Int>>.asString(
         { it.joinToString(separator, prefix, postfix) }
     )
 
+/**
+ * Creates a [Lens] from [List] of [Long] to [String]
+ */
 fun <P> Lens<P, List<Long>>.asString(
     separator: String = ", ",
     prefix: String = "",
@@ -82,6 +136,9 @@ fun <P> Lens<P, List<Long>>.asString(
         { it.joinToString(separator, prefix, postfix) }
     )
 
+/**
+ * Creates a [Lens] from [List] of [Double] to [String]
+ */
 fun <P> Lens<P, List<Double>>.asString(
     separator: String = ", ",
     prefix: String = "",
@@ -92,6 +149,9 @@ fun <P> Lens<P, List<Double>>.asString(
         { it.joinToString(separator, prefix, postfix) }
     )
 
+/**
+ * Creates a [Lens] from [List] of [Float] to [String]
+ */
 fun <P> Lens<P, List<Float>>.asString(
     separator: String = ", ",
     prefix: String = "",
@@ -102,6 +162,9 @@ fun <P> Lens<P, List<Float>>.asString(
         { it.joinToString(separator, prefix, postfix) }
     )
 
+/**
+ * Creates a [Lens] from [List] of [String] to [String]
+ */
 fun <P> Lens<P, List<String>>.asString(
     separator: String = ", ",
     prefix: String = "",
