f2941a0f8fff1b5105aa30dd86ffbe72b8e1e941
Christian Hausknecht <chausknecht@nundp.com>
2021-04-27 13:14:03 +0200

    Polish DataTable component
    - remove ``title`` property from header
    - adjust``colors.tertiary`` to brighter grays so they fit as good choices for data items backgrounds like in the DataTable
    - change hovering effect of DataTable to horizontal borders instead fo changing colors

--- components/src/jsMain/kotlin/dev/fritz2/components/datatable/context.kt
+++ components/src/jsMain/kotlin/dev/fritz2/components/datatable/context.kt
@@ -167,7 +167,7 @@ class ColumnsContext<T> {
         fun build(): Column<T> = Column(
             id.value,
             lens.value,
-            header.title,
+            title.value,
             width?.min?.value,
             width?.max?.value,
             hidden.value,
@@ -216,7 +216,6 @@ class ColumnsContext<T> {
          * ```
          */
         data class Header<T>(
-            val title: String = "",
             val styling: BasicParams.(sorting: Sorting) -> Unit = {},
             val content: Div.(column: Column<T>) -> Unit = { column ->
                 +column.title
@@ -225,14 +224,14 @@ class ColumnsContext<T> {
 
         var header: Header<T> = Header()
 
+        /**
+         * @see HeaderContext
+         */
         fun header(styling: BasicParams.(sorting: Sorting) -> Unit = {}, content: Div.(column: Column<T>) -> Unit) {
-            header = Header<T>(header.title, styling, content)
+            header = Header(styling, content)
         }
 
-        fun title(title: String) {
-            header = header.copy(title = title)
-        }
-
+        val title = ComponentProperty("")
         val hidden = ComponentProperty(false)
         val position = ComponentProperty(0)
 
@@ -267,6 +266,9 @@ class ColumnsContext<T> {
 
     val columns: MutableMap<String, Column<T>> = mutableMapOf()
 
+    /**
+     * @see ColumnContext
+     */
     fun column(
         styling: BasicParams.(value: IndexedValue<StatefulItem<T>>) -> Unit = {},
         expression: ColumnContext<T>.() -> Unit
@@ -278,6 +280,11 @@ class ColumnsContext<T> {
         }
     }
 
+    /**
+     * Kurzer Satz....
+     *
+     * @see ColumnContext
+     */
     fun column(
         styling: BasicParams.(value: IndexedValue<StatefulItem<T>>) -> Unit = {},
         title: String,
