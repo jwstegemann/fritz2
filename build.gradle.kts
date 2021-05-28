347d5d6b257e88580754dfe6bc06948666b6eec8
jan <jan@rexster>
2021-05-28 11:47:42 +0200

    upgrading to latest versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("multiplatform") version "1.5.0" apply false
-    kotlin("plugin.serialization") version "1.5.0" apply false
+    kotlin("multiplatform") version "1.5.10" apply false
+    kotlin("plugin.serialization") version "1.5.10" apply false
     id("org.jetbrains.dokka") version "1.4.32"
     id("maven-publish")
     signing
@@ -8,14 +8,15 @@ plugins {
 
 ext {
     // Dependencies
+    set("kotlinVersion", "1.5.10")
     set("coroutinesVersion", "1.5.0")
     set("kotlinpoetVersion", "1.8.0")
-    set("compileTestingVersion", "1.4.0")
+    set("compileTestingVersion", "1.4.1")
     set("stylisVersion", "4.0.2")
     set("murmurhashVersion", "1.0.0")
     set("logbackVersion", "1.2.1")
     set("ktorVersion", "1.4.0") // upgrade to 1.5 produces test errors
-    set("serializationVersion", "1.2.0")
+    set("serializationVersion", "1.2.1")
 }
 
 allprojects {
