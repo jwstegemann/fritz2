f4bf2331f7881e4f18ab182c74163eed66e94a98
Christian Hausknecht <chausknecht@nundp.com>
2021-07-02 10:18:43 +0200

    Final polishing typeAhead
    - extract common `InputField` properties into a property-interface and corresponding mixin
    - refactor `TypeAheadComponent` and `InputFieldComponent` to use this Mixin
    - add support for severity, readonly and disabling
    - finish API documentation compliant with issue #455

--- components/src/jsMain/kotlin/dev/fritz2/components/typeAhead.kt
+++ components/src/jsMain/kotlin/dev/fritz2/components/typeAhead.kt
@@ -8,11 +8,24 @@ import dev.fritz2.styling.StyleClass
 import dev.fritz2.styling.params.BasicParams
 
 /**
+ * The [typeAhead] factory function creates a [TypeAheadComponent].
+ *
+ * It offers the possibility to input some [String] and get some list of proposals to choose from.
+ * Internally this is achieved by adding some datalist to the input field
+ * (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/datalist).
+ *
+ * The typical (and minimal) usage might look like this:
+ * ```
+ * val proposals = listOf("Kotlin", "Scala", "Java", "OCaml", "Haskell").asProposals()
+ * val choice = storeOf("")
+ * typeAhead(value = choice, items = proposals) { }
+ * ```
  *
  * @see TypeAheadComponent
  *
  * @param styling a lambda expression for declaring the styling as fritz2's styling DSL
- * @param value optional [Store] that holds the data of the input
+ * @param value optional [Store] that holds the data of the result
+ * @param items
  * @param baseClass optional CSS class that should be applied to the element
  * @param id the ID of the element
  * @param prefix the prefix for the generated CSS class resulting in the form ``$prefix-$hash``
