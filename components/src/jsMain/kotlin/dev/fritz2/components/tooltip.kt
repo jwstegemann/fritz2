bfb88577cf8d61dff2a5eae86996f9eac209f376
WJA <jan.westphal-huusmann@oeffentliche.de>
2021-08-23 07:31:07 +0200

    rework kDoc - code examples

--- components/src/jsMain/kotlin/dev/fritz2/components/tooltip.kt
+++ components/src/jsMain/kotlin/dev/fritz2/components/tooltip.kt
@@ -24,8 +24,13 @@ fun StyleParams.tooltip(vararg text: String, tooltipPlacement: TooltipPlacements
  *
  * Example usage:
  * ```
- *   tooltip(text = "my Tooltip") { }
+ *   span {
+ *      +"hover me"
+ *      tooltip(text = "my Tooltip") { }
+ *   }
  *
+ *   span {
+ *   +"hover me"
  *   tooltip({
  *       color { danger.mainContrast }
  *       background {
@@ -54,8 +59,15 @@ fun RenderContext.tooltip(
  *
  * Example usage:
  * ```
- *   tooltip("my Tooltip") { }
- *   tooltip("my Tooltip") { placement { bottom } }
+ *   span {
+ *      +"hover me"
+ *      tooltip("my Tooltip") { }
+ *   }
+ *
+ *   span {
+ *      +"hover me"
+ *      tooltip("my Tooltip") { placement { bottom } }
+ *   }
  * ```
  *
  * @see TooltipComponent
@@ -74,7 +86,10 @@ fun RenderContext.tooltip(
  *
  * Example usage:
  * ```
- *   tooltip("my Tooltip", "second tooltip line") { }
+ *  span {
+ *      +"hover me"
+ *      tooltip("my Tooltip", "second tooltip line") { }
+ *  }
  * ```
  *
  *  @see TooltipComponent
