2b31c79c2e8ee7596b0323487fcf5051507b38bb
jan <jan@rexster>
2021-09-09 15:10:26 +0200

    use base58 instead of base62 for better readability

--- core/src/commonMain/kotlin/dev/fritz2/identification/id.kt
+++ core/src/commonMain/kotlin/dev/fritz2/identification/id.kt
@@ -2,7 +2,7 @@ package dev.fritz2.identification
 
 object Id {
     private const val defaultLength = 6
-    private val chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray()
+    private val chars = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ".toCharArray()
 
     fun next(length: Int = defaultLength) = buildString {
         for (i in 0 until length) {
