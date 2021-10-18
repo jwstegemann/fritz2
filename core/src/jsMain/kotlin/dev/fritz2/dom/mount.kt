22f53bf38e0299a220f55f4aea5465f36c29eac6
jwstegemann <jwstegemann@gmail.com>
2021-10-18 21:11:30 +0200

    fixed memory leak

--- core/src/jsMain/kotlin/dev/fritz2/dom/mount.kt
+++ core/src/jsMain/kotlin/dev/fritz2/dom/mount.kt
@@ -71,8 +71,8 @@ inline fun <V> TagContext.mount(
         }
     }
 
-    return target?.also { doMount(ProxyContext(target.job, target.scope, it)) }
-        ?: register(MountContext(job = this.job, scope = scope)) { doMount(it) }
+    return target?.also { doMount(ProxyContext(newJob, target.scope, it)) }
+        ?: register(MountContext(job = newJob, scope = scope)) { doMount(it) }
 }
 
 /**
