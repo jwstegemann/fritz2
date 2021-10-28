aa61ca125d09c47ed0d5d4056342a2e6980d6dfb
Christian Hausknecht <chausknecht@nundp.com>
2021-10-28 11:35:08 +0200

    Add workaround to fix annotation processor tests
    - add suggested fix from compiler testing lib: https://github.com/tschuchortdev/kotlin-compile-testing#java-16-compatibility
    - adopt proposal to kotlin gradle DSL

--- lenses-annotation-processor/build.gradle.kts
+++ lenses-annotation-processor/build.gradle.kts
@@ -37,4 +37,21 @@ kotlin {
     }
 }
 
+if (JavaVersion.current() >= JavaVersion.VERSION_16) {
+    tasks.withType<Test>().all {
+        jvmArgs(
+            "--add-opens=jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.jvm=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED",
+            "--add-opens=jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED",
+        )
+    }
+}
+
 apply(from = "$rootDir/publishing.gradle.kts")
