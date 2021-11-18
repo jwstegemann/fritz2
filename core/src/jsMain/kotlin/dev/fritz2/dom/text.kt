4d0f006869643020bff11f80ebf85265c4efe385
jwstegemann <jwstegemann@gmail.com>
2021-11-18 10:07:41 +0100

    use mountSimple for asText instead of complex mountpoint

--- core/src/jsMain/kotlin/dev/fritz2/dom/text.kt
+++ core/src/jsMain/kotlin/dev/fritz2/dom/text.kt
@@ -1,12 +1,14 @@
 package dev.fritz2.dom
 
+import dev.fritz2.binding.mountSimple
 import dev.fritz2.dom.html.RenderContext
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.dom.clear
 import org.w3c.dom.Node
 import org.w3c.dom.Text
-
 
 /**
  * Interface providing functionality to handle text-content
@@ -14,28 +16,39 @@ import org.w3c.dom.Text
 interface WithText<N : Node> : WithDomNode<N>, RenderContext {
 
     /**
+     * Mounts a [Flow] of [String]s to a [WithDomNode]
+     *
+     * @param target element to mount the [Flow] to
+     * @param upstream [Flow] to mount to [target]
+     */
+    fun mountTextNode(target: WithDomNode<*>, upstream: Flow<String>) {
+        mountSimple(job, upstream) {
+            target.domNode.clear()
+            target.domNode.appendChild(TextNode(it).domNode)
+        }
+    }
+
+    /**
      * Adds text-content of a [Flow] at this position
      *
+     * @param classes css-classes to apply to the generated span-element
+     * @param id id of the generated span-element
      * @receiver text-content
      */
     fun Flow<String>.asText(classes: String? = null, id: String? = null) =
         span(classes, id) {
-            mount(this, this@asText) {
-                domNode.appendChild(TextNode(it).domNode)
-            }
+            mountTextNode(this, this@asText)
         }
 
     /**
      * Adds text-content of a [Flow] at this position
      *
+     * @param classes css-classes to apply to the generated span-element
+     * @param id id of the generated span-element
      * @receiver text-content
      */
     fun <T> Flow<T>.asText(classes: String? = null, id: String? = null) =
-        span(classes, id) {
-            mount(this, this@asText) {
-                domNode.appendChild(TextNode(it.toString()).domNode)
-            }
-        }
+        this.map { it.toString() }.asText(classes, id)
 
     /**
      * Adds static text-content at this position
