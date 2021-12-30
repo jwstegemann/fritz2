67e356c1893a62db0ead5059398707a121aaaa5d
Christian Hausknecht <chausknecht@nundp.com>
2021-12-30 15:23:05 +0100

    Correct kdoc for lenses processor
    - change explanation from property based approach to function based one.
    - remove all arguments and advice against generic support

--- lenses-annotation-processor/src/jvmMain/kotlin/dev/fritz2/lenses/LensesProcessor.kt
+++ lenses-annotation-processor/src/jvmMain/kotlin/dev/fritz2/lenses/LensesProcessor.kt
@@ -17,49 +17,18 @@ import com.squareup.kotlinpoet.asClassName
 import com.squareup.kotlinpoet.ksp.*
 
 /**
- * This Processor generates automatically properties with lenses for all public properties of a data class within the
+ * This Processor generates automatically functions of lenses for all public properties of a data class within the
  * companion object of the data class.
  *
- * Those properties are created during compile process within separate source files as extension properties of the
+ * Those functions are created during compile process within separate source files as extension functions of the
  * companion object. That's why the user must provide a companion object within the `@Lenses` annotated data class
- * of his own. The processor will detect a missing definition and throw an error. The naming schema is based upon the
- * name of the data class with the appended suffix `Lenses`.
+ * of his own. The processor will detect a missing definition and throw an error. The naming schema  of the generated
+ * file is based upon the name of the data class with the appended suffix `Lenses`.
  *
- * We decided to model those generated lenses as properties, as the call to `buildLens` is always only performed once
- * in the lifetime of running program. So the client has not to deal with performance impacts and lifetime creation
- * considerations. (Think of calling a lens within a loop!)
- *
- * The former aspect is the main advantage compared to the other possible solution to model the lens generation as
- * functions!
- *
- * But this decision comes with a drawback:
- * As properties within a companion object cannot handle generic type parameters in Kotlin, there is currently no
- * support for generic type parameters!
- *
- * We believe that the need for a generic data class is rather a corner case and does therefore not outweigh the
- * performance advantage due to automatic lifetime optimization of the property based approach!
- *
- * We also discovered that one of the leading projects dealing with lenses [Arrow Optics](https://arrow-kt.io/docs/optics/)
- * "suffers" from the same problem: [Issue 2552](https://github.com/arrow-kt/arrow/issues/2552)
- * As this bug has been reported quite recently and does not gain much attraction, our considerations are further
- * reinforced.
- *
- * Nevertheless, if you really have some generic data class, you can use this template to create a working lens
- * factory method:
- * ```
- * data class Answer<T>(val value: T, val done: Boolean) {
- *     companion object {
- *         fun <T> value(): Lens<Answer<T>, T> = buildLens("value", { it.value }, { p, v -> p.copy(value = v)})
- *
- *         // also the "not" dependent property need to be modeled as `fun` as the first parameter of
- *         // `Lens` remains generic!
- *         fun <T> done(): Lens<Answer<T>, Boolean> = buildLens("done", { it.done }, { p, v -> p.copy(done = v)})
- *     }
- * }
- * ```
- * But have in mind that every call to those factories will create a new instance of a `Lens`, which is bad for
- * performance and temporarily memory consumptions! So we propose to create those lenses only once during lifetime
- * and store them appropriately.
+ * We decided to model those generated lenses as functions, as the call to `buildLens` has only a small impact to the
+ * overall rendering performance compared to other aspects but enables the support for *generic* data classes.
+ * So if a client suffers from a bad performance *because of this approach*, feel free to manually implement a lense
+ * with better performance.
  */
 class LensesProcessor(
     private val codeGenerator: CodeGenerator,
