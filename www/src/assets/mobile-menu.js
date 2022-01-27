bb81f504a3ad8b1012e0585a67ee47869fc3ab94
jwstegemann <jwstegemann@gmail.com>
2022-01-27 17:08:20 +0100

    change menu button-icon

--- www/src/assets/mobile-menu.js
+++ www/src/assets/mobile-menu.js
@@ -1,11 +1,16 @@
 console.log("geladen")
 
+const menu = document.getElementById("mobile-menu");
+const menuIcons = document.getElementsByClassName("mobile-icon")
+
 function toggleMenu() {
     console.log("toggle")
-    const menu = document.getElementById("mobile-menu");
     if (menu) {
         console.log("toggling")
         menu.classList.toggle("-translate-y-[100vh]")
+    }
+    for(let i = 0; i < menuIcons.length; i++) {
+        menuIcons[i].classList.toggle("hidden")
     }
 }
 
