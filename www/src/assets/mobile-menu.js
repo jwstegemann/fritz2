3227e0cbd21c044fcf6c80ecbe791bec303ea89c
jwstegemann <jwstegemann@gmail.com>
2022-01-27 13:07:25 +0100

    better scrolling + mobile menu

--- www/src/assets/mobile-menu.js
+++ www/src/assets/mobile-menu.js
@@ -5,7 +5,7 @@ function toggleMenu() {
     const menu = document.getElementById("mobile-menu");
     if (menu) {
         console.log("toggling")
-        menu.classList.toggle("hidden")
+        menu.classList.toggle("-translate-y-[100vh]")
     }
 }
 
