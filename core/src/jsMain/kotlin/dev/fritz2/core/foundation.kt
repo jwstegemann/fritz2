c6201c321830acb3f9de6e63641b180176f0759d
jamowei <jan@rexster.de>
2022-02-17 11:43:02 +0100

    added missing comments

--- core/src/jsMain/kotlin/dev/fritz2/core/foundation.kt
+++ core/src/jsMain/kotlin/dev/fritz2/core/foundation.kt
@@ -1,7 +1,14 @@
 package dev.fritz2.core
 
+/**
+ * Joins all given [classes] strings to one html-class-attribute [String]
+ * by filtering all out which are null or blank.
+ */
 fun classes(vararg classes: String?): String =
     classes.filter { !it.isNullOrBlank() }.joinToString(" ")
 
+/**
+ * Helper function to call a native js function with concrete return type [T]
+ */
 @JsName("Function")
 internal external fun <T> nativeFunction(vararg params: String, block: String): T
