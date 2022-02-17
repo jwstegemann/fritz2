23e0c8742bf210df24c302c52e7a00f76a4841e4
chausknecht <chausknecht@nundp.com>
2022-02-17 16:23:04 +0100

    Add further documentation

--- headless/src/jsMain/kotlin/dev/fritz2/headless/foundation/OpenClose.kt
+++ headless/src/jsMain/kotlin/dev/fritz2/headless/foundation/OpenClose.kt
@@ -7,7 +7,16 @@ import dev.fritz2.dom.html.shortcutOf
 import kotlinx.coroutines.flow.*
 import org.w3c.dom.HTMLElement
 
-
+/**
+ * Base class that provides all functionality needed for components, that have some "open" and "close" state of
+ * representation.
+ *
+ * Just extend from this class to gain and provide access to the basic data-binding [openClose] that holds the
+ * central state, the [opened] data-flow and expressive handler like [close] or [open] to set the state.
+ *
+ * Typical examples of [OpenClose] based components are modal dialogs or all popup-components, that appear and
+ * disappear based upon user interaction.
+ */
 abstract class OpenClose {
 
     val openClose = DatabindingProperty<Boolean>()
@@ -32,6 +41,13 @@ abstract class OpenClose {
         }
     }
 
+    /**
+     * Use this function on [Tag]s, that should trigger the component to open or to close in order to enable
+     * keyboard support. Applying this function will toggle the state by the keys `Space` and `Enter` like a
+     * `button` element behave natively.
+     *
+     * So it is usually a good idea to apply this feature, if the trigger is *not* a button!
+     */
     protected fun Tag<HTMLElement>.handleOpenCloseEvents() {
         openClose.handler?.invoke(openClose.data.flatMapLatest { state ->
             merge(
