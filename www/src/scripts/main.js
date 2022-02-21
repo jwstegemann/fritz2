f8828e0f5b40258c516a508e299d58be89ef6c96
jamowei <jan@rexster.de>
2022-02-21 16:04:34 +0100

    use 11ty templating instead of js import

--- www/src/scripts/main.js
+++ www/src/scripts/main.js
@@ -1,5 +1,1 @@
 import '../styles/main.css';
-import version from './version';
-
-// renders the version from version.js
-(() => { window.document.getElementById("f2-version").textContent = version; })();
