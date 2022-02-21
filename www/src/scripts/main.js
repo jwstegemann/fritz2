caa1147120b09b2d17580073afd00d2d4a68b08c
jamowei <jan@rexster.de>
2022-02-21 14:06:14 +0100

    add version from gradle to webpage

--- www/src/scripts/main.js
+++ www/src/scripts/main.js
@@ -1,1 +1,5 @@
 import '../styles/main.css';
+import version from './version';
+
+// renders the version from version.js
+(() => { window.document.getElementById("f2-version").textContent = version; })();
