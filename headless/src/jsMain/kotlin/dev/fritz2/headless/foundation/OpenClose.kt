a5624743bc706cafb986d788edcaeea1006ab277
chausknecht <chausknecht@nundp.com>
2022-02-21 13:23:12 +0100

    Polish API doc for OpenClose
    - remove obsolete logging code

--- headless/src/jsMain/kotlin/dev/fritz2/headless/foundation/OpenClose.kt
+++ headless/src/jsMain/kotlin/dev/fritz2/headless/foundation/OpenClose.kt
@@ -1,7 +1,10 @@
 package dev.fritz2.headless.foundation
 
 import dev.fritz2.core.*
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.merge
 
 /**
@@ -13,6 +16,12 @@ import kotlinx.coroutines.flow.merge
  *
  * Typical examples of [OpenClose] based components are modal dialogs or all popup-components, that appear and
  * disappear based upon user interaction.
+ *
+ * There are some protected functions in order to configure the appropriate actions for opening and closing based
+ * upon user interaction like pressing some keys or clicking with the mouse:
+ * - [toggleOnClicksEnterAndSpace]
+ * - [closeOnEscape]
+ * - [closeOnBlur]
  */
 abstract class OpenClose {
 
@@ -42,8 +51,6 @@ abstract class OpenClose {
      * Use this function on [Tag]s, that should trigger the component to open or to close in order to enable
      * keyboard support. Applying this function will toggle the state by the keys `Space` and `Enter` like a
      * `button` element behave natively.
-     *
-     * So it is usually a good idea to apply this feature, if the trigger is *not* a button!
      */
     protected fun Tag<*>.toggleOnClicksEnterAndSpace() {
         openClose.handler?.invoke(openClose.data.flatMapLatest { state ->
@@ -55,21 +62,27 @@ abstract class OpenClose {
                 it.preventDefault()
                 !state
             }
-        }.onEach {
-            console.log("toggling")
         })
     }
 
+    /**
+     * Apply this function on the panel representing [Tag] of the [OpenClose] implementing component, if the panel
+     * should be closed by pressing the *Escape* key.
+     */
     protected fun Tag<*>.closeOnEscape() {
         opened.flatMapLatest { isOpen ->
             Window.keydowns.filter { isOpen && shortcutOf(it) == Keys.Escape }
         } handledBy close
     }
 
+    /**
+     * Apply this function on the panel representing [Tag] of the [OpenClose] implementing component, if the panel
+     * should be closed on clicking to somewhere outside the panel.
+     */
     protected fun Tag<*>.closeOnBlur() {
         opened.flatMapLatest { isOpen ->
             Window.clicks.filter {
-                isOpen && it.composedPath().none { it == this}
+                isOpen && it.composedPath().none { it == this }
             }
         } handledBy close
     }
