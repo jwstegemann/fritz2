92f8b4f431dfa77e2151d038e898d60ae47fb86f
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2022-05-31 10:47:27 +0200

    upgrade to kotlin 1.6.21 and latest ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-    kotlin("multiplatform") version "1.6.10" apply false
-    kotlin("plugin.serialization") version "1.6.10" apply false
-    id("com.google.devtools.ksp") version "1.6.10-1.0.2" apply false
-    id("org.jetbrains.dokka") version "1.6.10"
+    kotlin("multiplatform") version "1.6.21" apply false
+    kotlin("plugin.serialization") version "1.6.21" apply false
+    id("com.google.devtools.ksp") version "1.6.21-1.0.5" apply false
+    id("org.jetbrains.dokka") version "1.6.21"
     id("maven-publish")
     signing
 }
@@ -16,14 +16,14 @@ rootProject.plugins.withType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJ
 // https://docs.gradle.org/current/userguide/platforms.html
 extra.apply {
     // Dependencies
-    set("kotlinVersion", "1.6.10")
-    set("coroutinesVersion", "1.6.0")
-    set("kotlinpoetVersion", "1.10.2")
+    set("kotlinVersion", "1.6.21")
+    set("coroutinesVersion", "1.6.1")
+    set("kotlinpoetVersion", "1.11.0")
     set("compileTestingVersion", "1.4.7")
     set("logbackVersion", "1.2.1")
     set("ktorVersion", "1.6.4") // upgrade to 1.6.5 produces test errors
     set("serializationVersion", "1.3.1")
-    set("kspVersion", "1.6.10-1.0.2")
+    set("kspVersion", "1.6.21-1.0.5")
     set("autoServiceVersion", "1.0.1")
     set("junitJupiterParamsVersion", "5.8.1")
     set("assertJVersion", "3.19.0")
