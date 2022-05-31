61abbc6c3d94d5fc9008615de52e84ccab3fd5e7
jwstegemann <jwstegemann@gmail.com>
2022-05-31 10:57:30 +0200

    restyled listbox demo

--- headless-demo/src/jsMain/kotlin/dev/fritz2/headlessdemo/listbox.kt
+++ headless-demo/src/jsMain/kotlin/dev/fritz2/headlessdemo/listbox.kt
@@ -29,23 +29,25 @@ fun RenderContext.listboxDemo() {
             value(bestCharacter)
             listboxLabel("sr-only", tag = RenderContext::span) { +"Choose the best Star Wars character" }
             listboxButton(
-                """flex items-center justify-end w-full py-2 pl-3 pr-3 text-left bg-white rounded-lg 
-                | shadow-md cursor-default focus:outline-none focus:ring-2 focus:ring-opacity-75 
-                | focus:ring-white focus:ring-offset-blue-700 focus:ring-offset-2 
-                | focus:border-blue-600 sm:text-sm""".trimMargin()
+                """flex items-center justify-end w-full py-2.5 px-4 text-left bg-white rounded
+                | cursor-default focus:outline-none focus:ring-4
+                | focus:ring-primary-600 border border-primary-600 font-sans text base
+                | focus:border-primary-800
+                  | text-primary-800""".trimMargin()
             ) {
                 span("block truncate w-full") {
                     value.data.renderText()
                 }
-                svg("w-5 h-5 text-gray-400") { content(HeroIcons.selector) }
+                icon("w-5 h-5 text-gray-600", content = HeroIcons.selector)
             }
 
             listboxItems(
-                """w-full py-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black 
-                    | ring-opacity-5 focus:outline-none sm:text-sm""".trimMargin(),
-                tag = RenderContext::ul
+                """w-full py-1 overflow-auto text-base bg-white rounded shadow-md max-h-60 ring-1 ring-primary-600 
+                    | ring-opacity-5 focus:outline-none origin-top divide-y divide-gray-100""".trimMargin(),
+                        tag = RenderContext::ul
             ) {
                 placement = Placement.bottomStart
+                distance = 5
 
                 transition(opened,
                     "transition duration-100 ease-out",
@@ -59,28 +61,28 @@ fun RenderContext.listboxDemo() {
                 characters.forEach { (entry, disabledState) ->
                     listboxItem(
                         entry,
-                        "w-full cursor-default select-none relative py-2 pl-10 pr-4",
+                        "w-full cursor-default select-none relative py-2 pl-10 pr-4 text-sm disabled:opacity-50",
                         tag = RenderContext::li
                     ) {
                         className(active.combine(disabled) { a, d ->
                             if (a && !d) {
-                                "text-blue-900 bg-blue-100"
+                                "bg-primary-600 text-white"
                             } else {
-                                if (d) "text-gray-300" else "text-gray-900"
+                                if (d) "text-slate-400" else "text-primary-800"
                             }
                         })
 
                         disable(disabledState)
 
                         span {
-                            className(selected.map { if (it) "font-medium" else "font-normal" })
+                            className(selected.map { if (it) "font-semibold" else "font-normal" })
                             +entry
                         }
 
                         selected.render {
                             if (it) {
-                                span("text-blue-600 absolute inset-y-0 left-0 flex items-center pl-3") {
-                                    svg("w-5 h-5") { content(HeroIcons.check) }
+                                span("absolute inset-y-0 left-0 flex items-center pl-3") {
+                                    icon("w-5 h-5", content = HeroIcons.check)
                                 }
                             }
                         }
@@ -94,8 +96,8 @@ fun RenderContext.listboxDemo() {
             }
         }
 
-        div("bg-gray-300 mt-4 p-2 rounded-lg ring-2 ring-gray-50", id = "result") {
-            em { +"Selected: " }
+        div("bg-primary-100 mt-4 p-2 rounded ring-2 ring-primary-500 text-sm text-primary-800 shadow-sm", id = "result") {
+            span("font-semibold") { +"Selected: " }
             span { bestCharacter.data.renderText() }
         }
     }
