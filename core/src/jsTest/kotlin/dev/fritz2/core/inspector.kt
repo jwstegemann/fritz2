e8535666a1b3a0a354843ba05a66b74cc81be8b3
Hauke Sommerfeld <development@haukesomm.de>
2023-06-20 11:31:00 +0200

    Add unit tests for `Inspector.mapNull()` and `Store.mapNull()`

---
+++ core/src/jsTest/kotlin/dev/fritz2/core/inspector.kt
@@ -0,0 +1,37 @@
+package dev.fritz2.core
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class NullableInspectorTests {
+
+    @Test
+    fun testMapNullResultsInSpecifiedDefaultValueWhenCalledOnNullContainingInspector() {
+        val rootInspector = inspectorOf<String?>(null)
+        val expected = "Foo"
+
+        val resultInspector = rootInspector.mapNull(expected)
+        val resultValue = resultInspector.data
+
+        assertEquals(
+            expected,
+            resultValue,
+            "Data of the derived Inspector must equal the expected value."
+        )
+    }
+
+    @Test
+    fun testPathsAreEqualForStoresAndInspectorsWhenDerivedViaMapNull() {
+        val rootInspector = inspectorOf<String?>(null)
+        val rootStore = storeOf<String?>(null)
+
+        val nonNullableInspector = rootInspector.mapNull("Test")
+        val nonNullableStore = rootStore.mapNull("Test")
+
+        assertEquals(
+            nonNullableStore.path,
+            nonNullableInspector.path,
+            "Sub inspector and sub Store paths must be the same."
+        )
+    }
+}
