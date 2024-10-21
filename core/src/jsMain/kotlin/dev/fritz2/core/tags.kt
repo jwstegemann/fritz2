jwstegemann/fritz2

commit cb5381460ed5febe78cb7090050b07f9b31b527d

Improves and enhances the reactive styling (#823)
* Improves and enhances the reactive styling

- removes `addToClasses` functions. Migration: Just rename all occurrences to `className`.
- substitutes the internal class name management with a `MutableStateFlow` based approach,
  where the former manages all different portions of the overall class names, so those can
  get merged appropriately and mounted only once into the tag's `class` attribute.
- add a new `className`-function variant: This generic function accepts some `Flow[T]`,
  which is then be used by some lamba expression to generate the class names.
  Additionally the calles must also provide some initial `T`, which is used to create the
  initial class names that gets set immediately to the DOM, without needing to wait for
  the flow's first value.
- add unit tests for new `className`-functions
- add and adapt documentation
- updates Playwright to version 1.40.1

---------

Co-authored-by: christian.hausknecht <christian.hausknecht@oeffentliche.de>
Co-authored-by: Metin Kale <metin.kale@oeffentliche.de>
Co-authored-by: Jan Weidenhaupt <jan.weidenhaupt@oeffentliche.de>

core/src/jsMain/kotlin/dev/fritz2/core/tags.kt
@@ -3,7 +3,9 @@ package dev.fritz2.core
 import kotlinx.browser.document
 import kotlinx.browser.window
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.plus
 import kotlinx.dom.clear
 import org.w3c.dom.Element
 import org.w3c.dom.Node
@@ -140,31 +142,35 @@ interface Tag<out E : Element> : RenderContext, WithDomNode<E>, WithEvents<E> {
     }
 
     /**
-     * adds a [String] of class names to the classes attribute of this [Tag]
-     */
-    fun addToClasses(classesToAdd: String)
-
-    /**
-     * adds a [Flow] of class names to the classes attribute of this [Tag]
-     */
-    fun addToClasses(classesToAdd: Flow<String>)
-
-    /**
-     * Sets the *class* attribute.
+     * Adds a [String] of class names to the classes attribute of this [Tag]
      *
      * @param value as [String]
      */
-    fun className(value: String) {
-        addToClasses(value)
-    }
+    fun className(value: String)
 
     /**
-     * Sets the *class* attribute.
+     * Adds a [Flow] of class names to the classes attribute of this [Tag]
      *
      * @param value [Flow] with [String]
      */
-    fun className(value: Flow<String>) {
-        addToClasses(value)
+    fun className(value: Flow<String>, initial: String = "")
+
+    /**
+     * Uses a [Flow] of [T] to create some class names by a [transform] lambda expression and add them to the classes
+     * attribute of the [Tag].
+     *
+     * In order to set some classes *immediately*, you must provide some initial [T], which is used to create the
+     * initial classes value with the [transform] lambda.
+     *
+     * Use this function, to avoid flickering effects on reactively based styling!
+     *
+     * @param value a [Flow] of [T] that provide the parameter for the [transform] lambda
+     * @param initial some [T] that should be used as initial state in order to generate and add class names
+     * immediately without waiting for the first value of the [Flow]
+     * @param transform a lambda expression, which finally creates class names by passing one [T]
+     */
+    fun <T> className(value: Flow<T>, initial: T, transform: (T) -> String) {
+        className(value.map(transform), transform(initial))
     }
 
     /**
@@ -173,7 +179,7 @@ interface Tag<out E : Element> : RenderContext, WithDomNode<E>, WithEvents<E> {
      * @param values as [List] of [String]s
      */
     fun classList(values: List<String>) {
-        addToClasses(values.joinToString(" "))
+        className(values.joinToString(" "))
     }
 
     /**
@@ -182,7 +188,7 @@ interface Tag<out E : Element> : RenderContext, WithDomNode<E>, WithEvents<E> {
      * @param values [Flow] with [List] of [String]s
      */
     fun classList(values: Flow<List<String>>) {
-        addToClasses(values.map { it.joinToString(" ") })
+        className(values.map { it.joinToString(" ") })
     }
 
     /**
@@ -192,7 +198,7 @@ interface Tag<out E : Element> : RenderContext, WithDomNode<E>, WithEvents<E> {
      * @param values as [Map] with key to set and corresponding values to decide
      */
     fun classMap(values: Map<String, Boolean>) {
-        addToClasses(values.filter { it.value }.keys.joinToString(" "))
+        className(values.filter { it.value }.keys.joinToString(" "))
     }
 
     /**
@@ -202,7 +208,7 @@ interface Tag<out E : Element> : RenderContext, WithDomNode<E>, WithEvents<E> {
      * @param values [Flow] of [Map] with key to set and corresponding values to decide
      */
     fun classMap(values: Flow<Map<String, Boolean>>) {
-        addToClasses(values.map { map -> map.filter { it.value }.keys.joinToString(" ") })
+        className(values.map { map -> map.filter { it.value }.keys.joinToString(" ") })
     }
 
     /**
@@ -362,27 +368,37 @@ open class HtmlTag<out E : Element>(
         return element
     }
 
-    private var className: String? = baseClass
-    private var classFlow: Flow<String>? = null
-
-    private fun updateClasses() {
-        if (classFlow == null) {
-            attr("class", className)
-        } else if (className == null) {
-            attr("class", classFlow!!)
-        } else {
-            attr("class", classFlow!!.map { classes(className, it) })
-        }
+    /**
+     * This [MutableStateFlow] acts as a backing field for all class names. It holds arbitrary [List]s of
+     * [StateFlow]s, in which each portion of class names get managed. So multiple calls of any [className] variant
+     * can be merged in one central place and mounted into the [Tag]s `class` attribute only once.
+     */
+    private val classesStateFlow by lazy {
+        MutableStateFlow<List<StateFlow<String>>>(listOfNotNull(baseClass?.let { MutableStateFlow(it) }))
+            .also { classesFlowList ->
+                attr("class", classesFlowList.flatMapLatest { styleFlows -> combine(styleFlows) { classes(*it) } })
+            }
     }
 
-    override fun addToClasses(classesToAdd: String) {
-        className = classes(className, classesToAdd)
-        updateClasses()
+    /**
+     * small utility function to create the classes [String] from the current values of the [StateFlow]s.
+     *
+     * This function is used to create the *initial* class name values, that should get applied *immediately*
+     * to the domnode.
+     */
+    private fun buildClasses() = classes(*classesStateFlow.value.map { it.value }.toTypedArray())
+
+    override fun className(value: String) {
+        classesStateFlow.value += MutableStateFlow(value)
+        // this ensures that the set state gets applied *immediately* without `Flow`-"delay"!
+        attr("class", buildClasses())
     }
 
-    override fun addToClasses(classesToAdd: Flow<String>) {
-        classFlow = if (classFlow == null) classesToAdd else classFlow!!.combine(classesToAdd) { a, b -> classes(a, b) }
-        updateClasses()
+    override fun className(value: Flow<String>, initial: String) {
+        classesStateFlow.value += value.stateIn(MainScope() + job, SharingStarted.Eagerly, initial)
+        // this ensures that the set state gets applied *immediately* without `Flow`-"delay"!
+        //  in this case, the `initial` value gets applied as "promised".
+        attr("class", buildClasses())
     }
 
     internal inner class AnnexContext : RenderContext {

core/src/jsTest/kotlin/dev/fritz2/core/tags.kt
@@ -1,15 +1,12 @@
 package dev.fritz2.core
 
+import dev.fritz2.renderWithJob
 import dev.fritz2.runTest
 import kotlinx.browser.document
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
-import org.w3c.dom.HTMLDivElement
-import org.w3c.dom.HTMLParagraphElement
-import org.w3c.dom.HTMLSpanElement
-import org.w3c.dom.asList
+import kotlinx.coroutines.flow.*
+import org.w3c.dom.*
 import kotlin.test.*
 
 
@@ -103,6 +100,7 @@ class TagTests {
         fun getSpanText(id: String) = (document.getElementById(id) as HTMLSpanElement).textContent
         fun getStaticSpan(id: String) =
             document.getElementById(id)!!.firstChild as HTMLSpanElement
+
         fun getStaticTextNode(id: String) = document.getElementById(id)!!.firstChild!!.firstChild
 
         data class Model(val static: String, val reactive: String)
@@ -136,7 +134,7 @@ class TagTests {
         assertEquals("RC-4", getSpanText(idValueReactive))
         val firstRenderedSpanCheck = getStaticTextNode(idMain)
         assertSame(firstRenderedSpan, firstRenderedSpanCheck)
-        assertEquals("fritz",  getStaticSpan(idMain).textContent)
+        assertEquals("fritz", getStaticSpan(idMain).textContent)
 
         // update with new value should trigger re-render
         store.update(Model("fritz2", "1.0-FINAL"))
@@ -264,6 +262,131 @@ class TagTests {
             assertEquals("li", element.localName)
             assertEquals(testClasses.joinToString(separator = " "), element.className, "wrong classes for $i")
         }
+    }
+
+    @Test
+    fun testStaticClassNameIsAppliedImmediately() = runTest {
+        renderWithJob {
+            div("ctor", id = Id.next()) {
+                className("immediately").also {
+                    assertEquals("ctor immediately", domNode.className)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun testClassNameWithInitialValue() = runTest {
+
+        val testId = Id.next()
+        val gate = storeOf(false)
+
+        val document = renderWithJob {
+            div("ctor", id = testId) {
+                className("fixed")
+                className(gate.data.transform { if (it) emit("visible") }, "invisible").also {
+                    // ensure initial class names are really applied *immediately*
+                    assertEquals("ctor fixed invisible", domNode.className)
+                }
+            }
+        }
+
+        delay(50)
+
+        val element = document.domNode.firstChild!! as HTMLDivElement
+
+        assertEquals(testId, element.id)
+        assertEquals("div", element.localName)
+        assertEquals("ctor fixed invisible", element.className)
+
+        gate.update(true)
+        delay(50)
+        assertEquals("ctor fixed visible", element.className)
+    }
+
+    @Test
+    fun testClassNameWithGenericFlowAndInitialValue() = runTest {
+
+        val testId = Id.next()
+        val gate = storeOf(false)
+        val statesToNames = mapOf(0 to "initial", 1 to "changed", 2 to "another")
+        val state = storeOf(-1)
+
+        val document = renderWithJob {
+            div("ctor", id = testId) {
+                className("fixed")
+                className(gate.data.transform { if (it) emitAll(state.data) }, 0) { statesToNames[it]!! }.also {
+                    // ensure initial class names are really applied *immediately*
+                    assertEquals("ctor fixed initial", domNode.className)
+                }
+            }
+        }
+
+        delay(50)
+
+        val element = document.domNode.firstChild!! as HTMLDivElement
+
+        assertEquals(testId, element.id)
+        assertEquals("div", element.localName)
+        assertEquals("ctor fixed initial", element.className)
+
+        // after initialization step, we allow state changes to appear within `className.value`
+        gate.update(true)
+
+        state.update(1)
+        delay(50)
+        assertEquals("ctor fixed changed", element.className)
+
+        state.update(2)
+        delay(50)
+        assertEquals("ctor fixed another", element.className)
+    }
+
+    @Test
+    fun testMultipleClassNameWithGenericFlowAndInitialValue() = runTest {
+
+        val testId = Id.next()
+        val gate = storeOf(false)
+        val fonts = mapOf(-1 to "none", 0 to "small", 1 to "medium", 2 to "large")
+        val stateOfFont = storeOf(-1)
+        val stateOfColor = storeOf("green")
+
+        val document = renderWithJob {
+            div("ctor", id = testId) {
+                className("fixed")
+                className(gate.data.transform { if (it) emitAll(stateOfFont.data) }, 0) { fonts[it]!! }.also {
+                    // ensure initial class names are really applied *immediately*
+                    assertEquals("ctor fixed small", domNode.className)
+                }
+                className(gate.data.transform { if (it) emitAll(stateOfColor.data) }, "red").also {
+                    // ensure initial class names are really applied *immediately*
+                    assertEquals("ctor fixed small red", domNode.className)
+                }
+            }
+        }
+
+        delay(50)
+
+        val element = document.domNode.firstChild!! as HTMLDivElement
+
+        assertEquals(testId, element.id)
+        assertEquals("div", element.localName)
+        assertEquals("ctor fixed small red", element.className)
+
+        // after initialization step, we allow state changes to appear within `className.value`
+        gate.update(true)
+
+        delay(50)
+        assertEquals("ctor fixed none green", element.className)
+
+        stateOfFont.update(1)
+        delay(50)
+        assertEquals("ctor fixed medium green", element.className)
+
+        stateOfFont.update(2)
+        stateOfColor.update("cyan")
+        delay(50)
+        assertEquals("ctor fixed large cyan", element.className)
     }
 
     @Test

core/src/jsMain/kotlin/dev/fritz2/core/mount.kt
@@ -126,8 +126,9 @@ internal class MountContext<T : HTMLElement>(
     override val domNode: HTMLElement = target.domNode
     override val id = target.id
     override val baseClass = target.baseClass
-    override fun addToClasses(classesToAdd: String) = target.addToClasses(classesToAdd)
-    override fun addToClasses(classesToAdd: Flow<String>) = target.addToClasses(classesToAdd)
+    override fun className(value: String) = target.className(value)
+    override fun className(value: Flow<String>, initial: String) = target.className(value, initial)
+
     override val annex: RenderContext = target.annex
 
     override val scope: Scope = Scope(mountScope).apply { set(MOUNT_POINT_KEY, this@MountContext) }
@@ -257,7 +258,12 @@ private fun insertOrAppend(target: Node, child: Node, index: Int) {
  * @param element from type [WithDomNode]
  * @param index place to insert or append
  */
-private suspend inline fun insert(target: Node, mountPoints: MutableMap<Node, MountPointImpl>, element: WithDomNode<*>, index: Int) {
+private suspend inline fun insert(
+    target: Node,
+    mountPoints: MutableMap<Node, MountPointImpl>,
+    element: WithDomNode<*>,
+    index: Int
+) {
     insertOrAppend(target, element.domNode, index)
     mountPoints[element.domNode]?.runAfterMounts()
 }
@@ -269,7 +275,12 @@ private suspend inline fun insert(target: Node, mountPoints: MutableMap<Node, Mo
  * @param elements [List] of [WithDomNode]s elements to insert
  * @param index place to insert or append
  */
-private suspend inline fun insertMany(target: Node, mountPoints: MutableMap<Node, MountPointImpl>, elements: List<WithDomNode<*>>, index: Int) {
+private suspend inline fun insertMany(
+    target: Node,
+    mountPoints: MutableMap<Node, MountPointImpl>,
+    elements: List<WithDomNode<*>>,
+    index: Int
+) {
     val f = document.createDocumentFragment()
     for (child in elements) {
         f.append(child.domNode)

core/src/jsTest/kotlin/dev/fritz2/test.kt
@@ -17,9 +17,9 @@ fun <T> runTest(block: suspend WithJob.() -> T): dynamic = MainScope().promise {
     job.cancelAndJoin()
 }
 
-fun WithJob.renderWithJob(content: RenderContext.() -> Unit) {
+fun WithJob.renderWithJob(content: RenderContext.() -> Unit) : HtmlTag<HTMLDivElement> =
     HtmlTag<HTMLDivElement>("div", job = job, scope = Scope()).apply(content)
-}
+
 
 fun <T> checkSingleFlow(
     done: CompletableDeferred<Boolean> = CompletableDeferred(),

examples/validation/src/commonMain/kotlin/dev/fritz2/examples/validation/validator.kt
@@ -3,10 +3,7 @@ package dev.fritz2.examples.validation
 import dev.fritz2.core.Lens
 import dev.fritz2.validation.ValidationMessage
 import dev.fritz2.validation.validation
-import kotlinx.datetime.Clock
-import kotlinx.datetime.LocalDate
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.todayAt
+import kotlinx.datetime.*
 
 enum class Status(val inputClass: String, val messageClass: String) {
     Valid("is-valid", "valid-feedback"),
@@ -34,7 +31,7 @@ val personValidator = validation<Person, Message> { inspector ->
 
     // validate the birthday
     val birthday = inspector.map(Person.birthday())
-    val today = Clock.System.todayAt(TimeZone.currentSystemDefault())
+    val today = Clock.System.todayIn(TimeZone.currentSystemDefault())
     when {
         birthday.data == LocalDate(1900, 1, 1) -> {
             add(Message(Person.id + birthday.path, Status.Invalid, "Please provide a birthday"))

headless/src/jsMain/kotlin/dev/fritz2/headless/components/listbox.kt
@@ -175,7 +175,7 @@ class Listbox<T, C : HTMLElement>(tag: Tag<C>, id: String?) : Tag<C> by tag, Ope
         "$componentId-items",
         scope,
         this@Listbox.opened,
-        reference = button,
+        reference = button ?: button { },
         ariaHasPopup = Aria.HasPopup.listbox
     ) {
 

headless/src/jsMain/kotlin/dev/fritz2/headless/components/menu.kt
@@ -103,7 +103,7 @@ class Menu<C : HTMLElement>(tag: Tag<C>, id: String?) : Tag<C> by tag, OpenClose
         "$componentId-items",
         scope,
         this@Menu.opened,
-        reference = button,
+        reference = button ?: button {  },
         ariaHasPopup = Aria.HasPopup.menu
     ) {
 

headless/src/jsMain/kotlin/dev/fritz2/headless/components/popOver.kt
@@ -73,7 +73,7 @@ class PopOver<C : HTMLElement>(tag: Tag<C>, id: String?) : Tag<C> by tag, OpenCl
         "$componentId-items",
         scope,
         this@PopOver.opened,
-        reference = button,
+        reference = button ?: button {  },
         ariaHasPopup = Aria.HasPopup.dialog
     )
 

commit 7a582ea8c1f3bb1f6c1a3e05e732f3b03180d187

Fixes some potential bug with mapping lenses
The pimped `className`-function (introduced by PR #823 in `1.0-RC15` release) did not apply exception handling. That could lead to errors,
if there is code that removes some element:

The independent reactive renderings could simply lead to corner cases,
where a lens will try to access its mapped object (e.g. by `mapByElement`)
that is no longer part of the stored model. So a
`CollectionLensGetException` will be thrown and must be handled by the
consuming code.

This PR adds such exception handling code and a unit test to verify
the new, correct behaviour.

core/src/jsMain/kotlin/dev/fritz2/core/tags.kt
@@ -361,7 +361,10 @@ open class HtmlTag<out E : Element>(
     }
 
     override fun className(value: Flow<String>, initial: String) {
-        classesStateFlow.value += value.stateIn(MainScope() + job, SharingStarted.Eagerly, initial)
+        classesStateFlow.value += value
+            .catch { printErrorIgnoreLensException(it) }
+            .stateIn(MainScope() + job, SharingStarted.Eagerly, initial)
+
         // this ensures that the set state is applied *immediately* without `Flow`-"delay".
         // in this case, the `initial` value gets applied as "promised".
         attr("class", buildClasses())

core/src/jsTest/kotlin/dev/fritz2/core/tags.kt
@@ -543,4 +543,49 @@ class TagTests {
         assertEquals("fourth", getAttribute())
     }
 
+    @Test
+    fun testClassNameWithFlowInsideReactiveScopeBasedUponListMapping_WontThrowUnhandledCollectionLensGetException() =
+        runTest {
+            val items = listOf("red", "green", "blue")
+
+            fun extractClasses() = items.mapNotNull { item -> document.getElementById(item) }
+                .joinToString(",") { it.className }
+
+            val storedItems = storeOf(items, Job())
+            val removeItem = storedItems.handle<String> { allItems, toRemove ->
+                allItems.filterNot { it == toRemove }
+            }
+
+            render {
+                storedItems.data.renderEach { item ->
+                    val storedItem = storedItems.mapByElement(item) { it }
+                    div(id = item) {
+                        // access to mapped item will throw `CollectionLensGetException` after deletion
+                        // -> must be catched internally like handlers do!
+                        className(storedItem.data)
+
+                        // we give some time to the `className`-function to try to access a mapped lens for an object
+                        // that might not exist anymore.
+                        beforeUnmount { _, _ -> delay(100) }
+                    }
+                }
+            }
+
+            delay(100)
+            assertEquals("red,green,blue", extractClasses())
+
+            // this is the source of possible problem: By removing mapped lenses won't find their targeting object
+            // anymore. This will lead to `CollectionLensGetException`, that must be handled internally!
+            removeItem("green")
+
+            delay(200)
+            assertEquals("red,blue", extractClasses())
+
+            removeItem("blue")
+
+            // if not exception handling on the `classes`-Flow is done, rendering will freeze and the node for green
+            // will remain.
+            delay(200)
+            assertEquals("red", extractClasses())
+        }
 }